b0df776fb73fa5ffccab52a21fc9971a9b3400b2
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-03-27 20:55:36 +0900

    Add files via upload

---
+++ 소스.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*남민경, 2016113328, 210326*/
+	/*printf("Hi! Programmers\n");
+	printf("\nGood Morning\n");
+	printf("C언어\nC++언어\nJava 언어\n\n");
+
+
+	printf("이름: 홍길동\n");
+	printf("나이: 21살\n");
+	printf("주소: 서울 200번지\n\n");
+
+	printf("Hello\nC\nProgrammers!\n\n");
+
+	printf("**********************************************************\n");
+	printf("*        리포트        *\n");
+	printf("**********************************************************\n");
+	printf("     ㅡㅡㅡㅡㅡㅡ\n");
+	printf("    ㅣ컴퓨터학과ㅣ\n");
+	printf("     ㅡㅡㅡㅡㅡㅡ\n");
+	printf("     20070001\n");
+	printf("     홍길동\n\n");*/
+	
+	/*printf("일\t월\t화\t수\t목\t금\t토\n");
+	printf("1\t2\t3\t4\t5\t6\t7\n");
+	printf("8\t9\t10\t11\t12\t13\t14\n");
+	printf("15\t16\t17\t18\t19\t20\t21\n");
+	printf("22\t23\t24\t25\t26\t227\t28\n");
+	printf("29\t30\t31\n");*/
+
+	/*//변수 선언
+	int A;
+	int B;
+	int sum;
+
+	//입력값
+	A = 1893;
+	B = 265;
+
+	//수식
+	sum = A * B;
+	printf("%d", sum); //흑흑 이 마지막 문장은 책을 봐버림 흑흑*/
+
+	//중간점검 p.93
+	int a;
+	int b;
+	int product;
+	int quotient;
+
+	a = 100;
+	b = 5;
+
+	product = a * b;
+	quotient = a / b;
+
+	printf("%d\n", product);
+	printf("%d", quotient);
+
+	return 0;
+}
