0b1b252ff8cc2401febb98e0e3827715cdfad3e8
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-06 22:10:53 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,49 +1,65 @@
 #include <stdio.h>
 
 /*
-int sum(int num1, int num2)
+//제곱 구하기
+int square(int n)
 {
-	int result;
-
-	result = num1 + num2;
-	return result;
+	return (n * n);
 }
 
 int main(void)
 {
-	int num1, num2, result;
+	int n;
 
-	scanf_s("%d %d", &num1, &num2);
-
-	result = sum(num1, num2);
-	printf("%d + %d = %d", num1, num2, result);
+	printf("숫자를 입력하세요: ");
+	scanf_s("%d", &n);
+	
+	printf("%d의 제곱은 %d입니다.", n, square(n));
 
 	return 0;
 }
 
 
-int put_num(void)
+//큰 수 구하기
+int get_max(int x, int y)
 {
-	int student_num;
-
-	scanf_s("%d", &student_num);
-	return student_num;
-}
-
-int out_num(int student_num)
-{
-	printf("%d", student_num);
-	return student_num;
+	if (x > y)
+		return x;
+	else
+		return y;
 }
 
 int main(void)
 {
-	int student_num;
+	int x, y;
 
-	student_num = put_num();
+	printf("정수 두 개를 입력하세요: ");
+	scanf_s("%d %d", &x, &y);
 	
-	out_num(student_num);
-	
+	printf("두 수 중 큰 수는 %d입니다.", get_max(x, y));
+
 	return 0;
 }
 */
+
+void draw_rect(int side)
+{
+	int x, y;
+
+	for (y = 0; y < side; y++)
+	{
+		for (x = 0; x < side; x++)
+			printf("*");
+		printf("\n");
+	}
+	return;
+}
+
+int main(void)
+{
+	int side;
+
+	scanf_s("%d", &side);
+	printf("", draw_rect(side));
+}
+
