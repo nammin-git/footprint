4a27b5dcc3ff4d1d04dcaed80b7c41422bd11189
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-07 23:57:08 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,65 +1,19 @@
 #include <stdio.h>
 
-/*
-//제곱 구하기
-int square(int n)
+int add(int x, int y)
 {
-	return (n * n);
+	return (x + y);
 }
 
 int main(void)
 {
-	int n;
+	int sum;
 
-	printf("숫자를 입력하세요: ");
-	scanf_s("%d", &n);
-	
-	printf("%d의 제곱은 %d입니다.", n, square(n));
+	sum = add(2, 3);
+	printf("%d\n", sum);
+
+	sum = add(5, 6);
+	printf("%d\n", sum);
 
 	return 0;
 }
-
-
-//큰 수 구하기
-int get_max(int x, int y)
-{
-	if (x > y)
-		return x;
-	else
-		return y;
-}
-
-int main(void)
-{
-	int x, y;
-
-	printf("정수 두 개를 입력하세요: ");
-	scanf_s("%d %d", &x, &y);
-	
-	printf("두 수 중 큰 수는 %d입니다.", get_max(x, y));
-
-	return 0;
-}
-*/
-
-void draw_rect(int side)
-{
-	int x, y;
-
-	for (y = 0; y < side; y++)
-	{
-		for (x = 0; x < side; x++)
-			printf("*");
-		printf("\n");
-	}
-	return;
-}
-
-int main(void)
-{
-	int side;
-
-	scanf_s("%d", &side);
-	printf("", draw_rect(side));
-}
-
