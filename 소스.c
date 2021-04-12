d469472061793421734db40f089586dc6f5bd244
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-12 23:57:18 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,49 +1,91 @@
 #include <stdio.h>
 
+/*
+#define SEC_PER_MINUTE 60
+
 int main(void)
 {
-	int i = 5;
-	printf("%d\n", i++ * i++);
-	//출력은 25 출력 후 i 값은 6
 
-	((x < y) ? x : y) = 100;
-	//불가능
+	int input, minute, second;
+
+	printf("초를 입력하시오: ");
+	scanf_s("%d", &input);
+
+	minute = input / SEC_PER_MINUTE;
+	second = input % SEC_PER_MINUTE;
+
+	printf("%d초는 %d분 %d초 입니다.\n\n", input, minute, second);
+
+
+	int x = 10, y = 10;
+	printf("x=%d\n", x);
+	printf("++x=%d\n", ++x);
+	printf("x=%d\n\n", x);
+
+	printf("y=%d\n", y);
+	printf("y++=%d\n", y++);
+	printf("y=%d\n", y);
+	
+
+	int i;
+	double f;
+
+	f = 5 / 4;
+	printf("%f\n", f);
+
+	f = (double)5 / 4;
+	printf("%f\n", f);
+
+	f = 5.0 / 4;
+	printf("%f\n", f);
+
+	f = (double)5 / (double)4;
+	printf("%f\n", f);
+
+	i = 1.3 + 1.8;
+	printf("%d\n", i);
+
+	i = (int)1.3 + (int)1.8;
+	printf("%d\n", i);
+
+	
+
+	int x, y;
+
+	printf("두 개의 정수를 입력하시오: ");
+	scanf_s("%d%d", &x, &y);
+
+	printf("x==y의 결과값: %d\n", x == y);
+	printf("x!=y의 결과값: %d\n", x != y);
+	printf("x>y의 결과값: %d\n", x > y);
+	printf("x<y의 결과값: %d\n", x < y);
+	printf("x>=y의 결과값: %d\n", x >= y);
+	printf("x<=y의 결과값: %d\n", x <= y);
+
 
 	return 0;
 }
 
+*/
+
+
+int leapyear(int year)
+{
+	(year%4)&&
+
+
+	return result;
+}
 
 int main(void)
 {
-	int x = 1, y = 2, z = 3;
-	double f = 0.0;
+	int year;
 
-	printf("%d\n", 3 / 5);
-	//출력되는 값은 0
-	printf("%d\n", 3 % 5);
-	//출력되는 값은 3
-	y *= x;
-	//y = y * x;
-	//y = 2 * 1;
-	x++;
-	//x의 값을 출력한 뒤 x는 1만큼 증가
-	//출력은 1, 출력한 뒤 x = 2
-	printf("%f\n", 2.0 / 4);
-	//값은 0.5
+	printf("연도를 입력하시오: ");
+	scanf_s("%d", &year);
 
-	x = 12.3;
-	//x는 int형이므로 x에 저장되는 값은 12
-	f = (double)x;
-	//x를 double형으로 변환
+	printf("result=%d", leapyear(year));
 
-	printf("%d\n", 10 > 3);
-	//(10>3)은 참이므로 출력되는 값은 1
-	printf("%d\n", (10 > 3) && (1 > 10));
-	//(1>10)이 거짓이므로 출력되는 값은 0
-	printf("%d\n", (10 < 3) || (1 > 10));
-	//(10<3), (1>10) 둘 다 거짓이므로 출력되는 값은 0
-	x = y = z = 10;
-	//연산 순서?ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 	return 0;
 }
