7af3d151fd981bb78f42943d8383a43a199eed4f
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-13 23:52:04 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,77 +1,16 @@
 #include <stdio.h>
 
-/*
-#define SEC_PER_MINUTE 60
-
-int main(void)
-{
-
-	int input, minute, second;
-
-	printf("초를 입력하시오: ");
-	scanf_s("%d", &input);
-
-	minute = input / SEC_PER_MINUTE;
-	second = input % SEC_PER_MINUTE;
-
-	printf("%d초는 %d분 %d초 입니다.\n\n", input, minute, second);
-
-
-	int x = 10, y = 10;
-	printf("x=%d\n", x);
-	printf("++x=%d\n", ++x);
-	printf("x=%d\n\n", x);
-
-	printf("y=%d\n", y);
-	printf("y++=%d\n", y++);
-	printf("y=%d\n", y);
-	
-
-	int i;
-	double f;
-
-	f = 5 / 4;
-	printf("%f\n", f);
-
-	f = (double)5 / 4;
-	printf("%f\n", f);
-
-	f = 5.0 / 4;
-	printf("%f\n", f);
-
-	f = (double)5 / (double)4;
-	printf("%f\n", f);
-
-	i = 1.3 + 1.8;
-	printf("%d\n", i);
-
-	i = (int)1.3 + (int)1.8;
-	printf("%d\n", i);
-
-	
-
-	int x, y;
-
-	printf("두 개의 정수를 입력하시오: ");
-	scanf_s("%d%d", &x, &y);
-
-	printf("x==y의 결과값: %d\n", x == y);
-	printf("x!=y의 결과값: %d\n", x != y);
-	printf("x>y의 결과값: %d\n", x > y);
-	printf("x<y의 결과값: %d\n", x < y);
-	printf("x>=y의 결과값: %d\n", x >= y);
-	printf("x<=y의 결과값: %d\n", x <= y);
-
-
-	return 0;
-}
-
-*/
-
 
 int leapyear(int year)
 {
-	(year%4)&&
+	int result;
+
+	result = ((year % 4) == 0) ? 1 : 0;
+
+	if ((year % 400) == 0)
+		return 1;
+	if ((year % 100) == 0)
+		return 0;
 
 
 	return result;
