f8bdc4298673aee99afe214f1a32e1c0a48b9c76
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-17 22:17:22 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,125 +1,141 @@
 #include <stdio.h>
 
+void hello(void)
+{
+	printf("HELLO!\n");
+
+}
+
+void printNum(int x)
+{
+	printf("Num : %d\n", x);
+	return;
+}
+
+int getQuantity(void)
+{
+	int qty;
+	printf("Enter Quantity : ");
+	scanf_s("%d", &qty);
+	return qty;
+}
+
+int sqr(int x)
+{
+	return x * x;
+}
+
+
+
 int main(void)
 {
-	printf("미디어소프트웨어학부\n2학년\n박예랑\n");
-
-	int x = 123;
-	float y = 12.24;
-	char z = 'A';
-
-	printf("정수 : %d\n", x);
-	printf("실수: %f\n", y);
-	printf("문자 : %c\n", z);
-
 	
-	int id;
+	int a;
 
-	printf("학번을 입력해주세요: ");
-	scanf_s("%d", &id);
-	printf("당신의 학번은 %d입니다.", id);
+	scanf_s("%d\n\n", &a);
+
+	(a > 5) ? printf("1") : printf("2");
 
 
-	
-	//입력버퍼
+	int b = 5;
 
-	int id, age;
-	char gender;
-
-	printf("학번을 입력해주세요: ");
-	scanf_s("%d", &id);
-
-	printf("나이을 입력해주세요: ");
-	scanf_s("%d", &age);
-
-	while (getchar() != '\n');
-	printf("성별을 입력해주세요:(남: m, 여: f) ");
-	scanf_s("%c", &gender);
-
-	printf("학번:%d, 나이: %d, 성별: %c\n", id, age, gender);
-
-	
-
-	//나눗셈을 이용한 프로그램
-
-	int num1, num2, div, rest;
-
-	printf("두 수를 입력해주세요: ");
-	scanf_s("%d %d", &num1, &num2);
-
-	div = num1 / num2;
-	rest = num1 % num2;
-
-	printf("몫: %d, 나머지: %d", div, rest);
+	printf("논리곱(AND) : b는 3보다 크고 10보다 작다 : %d\n", (b > 3) && (b < 10));
+	printf("논리곱(AND) : b는 3보다 크고 5보다 작다 : %d\n", (b > 3) && (b < 5));
+	printf("논리합(OR) : b는 5이거나 4다 : %d\n", (b == 5) || (b == 4));
+	printf("논리합(OR) : b는 6이거나 7다 : %d\n\n", (b == 6) || (b ==7));
 
 
 
-	//4시간 30분을 초로 바꾸기
+	int a, b;
 
-	int hour, minute, second;
+	printf("a와 b의 값을 입력해주세요. ");
+	scanf_s("%d %d", &a, &b);
 
-	printf("시간을 입력하세요: ");
-	scanf_s("%d", &hour);
+	(a == b) ? printf("1") : printf("2");
 
-	printf("분을 입력하세요: ");
-	scanf_s("%d", &minute);
-
-	second = hour * 60 * 60 + minute * 60;
-
-	printf("%d시간 %d분은 %d초 입니다.\n", hour, minute, second);
+	//AND는 어디에 사용하죠..? ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 
+	printf("안녕하세요\n");
+	hello();
 
 
-	//필드 폭 지정
-
-	double x = 12.2345678911;
-	int y = 10;
-
-	printf("%190d\n", y);
-	printf("%-190d\n", y);
-	printf("%0900d\n", y);
-	printf("%.8lf\n", x);
-	
-	//형변환
-	printf("정수부분만 출력 x=%d\n", (int)x);
-	printf("소수부분까지 출력 x=%lf\n", (float)y);
-
-	
-	//영수증 출력 프로그램
-
-	int _1_ea, _1_price, _2_ea, _2_price, _3_ea, _3_price;
-	int total;
-
-	printf("당근의 개수와 가격을 입력하세요: ");
-	scanf_s("%d %d", &_1_ea, &_1_price);
-
-	printf("토마토의 개수와 가격을 입력하세요: ");
-	scanf_s("%d %d", &_2_ea, &_2_price);
-
-	printf("오이의 개수와 가격을 입력하세요: ");
-	scanf_s("%d %d", &_3_ea, &_3_price);
+	int a = 5;
+	printNum(a);
 
 
-	printf("\n\n=================================\n");
+	int amt;
+	amt = getQuantity();
 
-	printf("\t영수증\n");
-	printf("번호\t품명\t개수\t가격\t\n");
-	printf("1\t당근\t%d\t%d\t\n", _1_ea, _1_price);
-	printf("2\t토마토\t%d\t%d\t\n", _2_ea, _2_price);
-	printf("3\t오이\t%d\t%d\t\n", _3_ea, _3_price);
-	
-	
-	
-	printf("\n\n---------------------------------\n");
-	
-	total = _1_ea * _1_price + _2_ea * _2_price + _3_ea * _3_price;
+	printf("amt : %d\n", amt);
 
-	printf("총합 : %d\n", total);
 
-	printf("=================================");
+	int b, c;
 
+	scanf_s("%d", &b);
+	c = sqr(b);
+	printf("input : %d\nsquared : %d \n", b, c);
 
 
 	return 0;
 }
+
+
+int add(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+
+int main(void)
+{
+	int num1, num2;
+
+	printf("두 개의 숫자를 입력하시오: ");
+	scanf_s("%d %d", &num1, &num2);
+
+	printf("result = %d", add(num1, num2));
+
+	return 0;
+}
+
+
+float sqr(float a, float b)
+{
+	return a * b;
+}
+
+int main(void)
+{
+	float width, height;
+
+	printf("계산할 사각형의 가로와 세로를 입력하세요: ");
+	scanf_s("%f %f", &width, &height);
+
+	printf("사각형의 넓이는 %f입니다.", sqr(width, height));
+
+
+	return 0;
+}
+
+
+int main(void)
+{
+
+	int i = -3, j = 2, k = 0, m;
+	m = ++i && ++j && ++k;
+
+	printf("%d, %d, %d, %d\n", i, j, k, m);
+
+
+	int x = 4, y, z;
+	y = --x;
+	z = x--;
+	printf("%d, %d, %d\n", x, y, z);
+
+
+	printf("%x\n", 0x20 << 2);
+
+	return 0;
+}
+
