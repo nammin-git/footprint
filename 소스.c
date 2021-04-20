a78c247fcf3ae17bf9399c82c853d8d058da8b8a
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-20 12:47:34 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,84 +1,34 @@
 #include <stdio.h>
 
-//문제1. 학점 출력 프로그램
-
 int main(void)
 {
-	int kor, eng, math, avg;
+	//if문은 조건식이 참일 때만 실행
+	int num = 5;
 
-	printf("국어, 영어, 수학 점수를 입력해주세요:\n");
-	scanf_s("%d %d %d", &kor, &eng, &math);
-
-	avg = (kor + eng + math) / 3;
-
-	if (avg >= 90)
-		printf("A 학점");
-	else if (avg >= 80)
-		printf("B 학점");
-	else if (avg >= 70)
-		printf("C 학점");
-	else
-		printf("D 학점");
-
-	return 0;
-}
+	if (num > 0)
+		printf("양수입니다.\n");
 
 
-//문제2. 5의 판별 프로그램
+	int temperature = 17;
 
-int main(void)
-{
-	int x;
+	if (temperature < 0)
+		printf("영하입니다.\n");
 
-	printf("1부터 100까지의 정수를 입력하세요: ");
-	scanf_s("%d", &x);
 
-	if ((x > 100) || (x < 1))
-		return 0;
-	//exit()?
-	else if ((x % 5) == 0)
-		printf("%d는 5의 배수입니다.\n", x);
-	else
-		printf("%d는 5의 배수가 아닙니다.\n", x);
+	//if1.c 양수입니다
+	int number;
+	
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &number);
+	printf("입력된 값은 %d입니다.\n", number);
+
+	if (number > 0)
+		printf("양수입니다.\n");
+
+
+	//if2.c 절대값 구하기
+
+
 
 	return 0;
 }
-
-
-//문제3. 계산기 프로그램
-
-int calculator(int x, int y)
-{
-	int z;
-
-	switch (z)
-		case x+y:
-			printf("result : %d", x+y);
-			break;
-		case x-y:
-			printf("result : %d", x-y);
-			break;
-		case x*y:
-			printf("result : %d", x*y);
-			break;
-		case x/y:
-			printf("result : %d", x/y);
-			break;
-
-//왜 굳이 switch-case 문을 써야하지...?
-	return 0;
-}
-
-
-int main(void)
-{
-	int num1, num2, result;
-
-	printf("연산식을 입력해주세요. (예시 : 2+3)\n");
-	scanf_s("%d %d", &num1, &num2);
-
-	result = calculator(num1, num2);
-
-	return 0;
-}
-
