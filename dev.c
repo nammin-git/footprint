9f723db04aae753cf40cc3f79bffbe1e7290ffbd
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-22 23:55:40 +0900

    Update dev.c

--- dev.c
+++ dev.c
@@ -1,93 +1,96 @@
 #include <stdio.h>
-
-//사칙연산 계산기
-
-int sum()
+/*
+//시험성적 91~100 까지 A 
+void main(void)
 {
-	int num1;
-	int num2;
+	int score;
+	int iscore, jscore;
 	
-	return num1+num2;
+	scanf_s("%d", &score);
+	
+	(score >= 0) || (score <= 100);
+	
+	iscore = (score%10)!=0
+	jscore = (score%10)==0
+	
+	switch(iscore)
+	case 100:
+		case
+	
 }
 
 
+//윤년
 int main(void)
 {
-	int num1, num2;
+	int year;
 	
-	printf("숫자 두 개를 입력하시오.(얘시 2 3)");
-	scanf("%d %d, &num1, &num2"); 
+	scanf("%d", &year);	
 	
-	printf("sum()");
-	//printf("%d+%d=%d", num1, num2, sum());
+	if (year%4==0)
+	{
+		if (year%100==0)
+			if(year%400==0)
+				printf("1");
+			else
+				printf("0");
+		else
+			printf("1");
+	}
+	else
+		printf("0");
 	
-	return 0;
-}
-
-
- //별 10개 출력하기
-int main(void)
-{
-	int i;
-	for (i=0; i<10; i++)
-	printf("*");
-	
-	return 0;
+return 0;
 } 
+*/
 
-
-void print_star()
-{
-	int i;
-	for(i=0; i<10; i++)
-	printf("*");
-}
-
+//사분면 고르기
 int main(void)
 {
-	print_star();
-	print_star();
-	print_star();
-	print_star();
-	print_star();
-	print_star();
-	print_star();
-	print_star();
-	print_star();
-	print_star();
-	print_star();
+	int x, y;
 	
+	scanf("%d %d", &x, &y);
+	//두 줄에 출력 불가능? 
+/*	
+	if ((x>0)&&(x<1000)){
+		if ((y>0)&&(y<=1000))
+			printf("1");
+		else if ((y<0)&&(y>-1000))
+			printf("4");
+		else
+			return 0;
+	}
+	else if ((x<0)&&(x>-1000)){
+		if ((y>0)&&(y<=1000))
+			printf("2");
+		else if ((y<0)&&(y>-1000))
+			printf("3");
+		else
+			return 0;
+	}
+	else
+		return 0;
+*/	
+
+if (X>0)
+{
+	if (y>0)
+	printf("1사분면");
+	else if (y<0) 
+	printf("4사분면");
+	else
 	return 0;
 }
-
-//x+y+z 반환하는 문장 작성
-int add_triple(int x, int y, int z)
+else if (x<0)
 {
-	return x+y+z;
-}
-
-int main(void)
-{
-	int x=1;
-	int y=3;
-	int z=5;
-	
-	printf("%d", add_triple(x, y, z));
-
-	
+	if (y>0)
+	printf("2사분면");
+	else if (y<0) 
+	printf("3사분면");
+	else
 	return 0;
 }
-
-
-//fabs 헤더함수 정의
-double fabs(double f)
-{
-	return ;
-}
-
-
-
-
-
-
+	
+return 0;
+} 
 
