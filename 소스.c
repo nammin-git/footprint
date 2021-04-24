e69e45f2aa08bed6640dbcbadacbb770ded23884
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-24 22:44:20 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,65 +1,92 @@
 #include <stdio.h>
 
-int calculator(int num1, char cal, int num2);
+void early(int H, int M);
 
-int main(void)
+void main(void)
 {
-	//문제2
-	int number;
+	//알람 시계 1
+	int H, M;
 
-	printf("1부터 100까지 정수를 입력하세요: ");
-	scanf_s("%d", &number);
+	scanf_s("%d %d", &H, &M);
 
-	if ((number >= 1) && (number <= 100))
-	{
-		if (number % 5 == 0)
-			printf("%d은(는) 5의 배수입니다.\n", number);
+	//if ((M >= 0) || (M <= 59))
+	//	if ((H >= 0) || (H <= 23))
+	if (H != 0)
+		if (M >= 45)
+			printf("%d %d", H, (M - 45));
 		else
-			printf("%d은(는) 5의 배수가 아닙니다.\n", number);
-	};
+			printf("%d %d", (H - 1), (M + 60 - 45));
+	else
+		if (M >= 45)
+			printf("%d %d", (H), (M - 45));
+		else
+			printf("%d %d", (H + 24 - 1), (M + 60 - 45));
 
 
-	//문제3
-	int num1, num2;
-	char cal;
-	int result;
+	//알람 시계 2
+	int H, M;
 
-	printf("연산시을 입력해주세요. (예시: 2+3)\n");
-	scanf_s("%d%c%d\n", &num1, &cal, &num2);
-
-	result = calculator(num1, cal, num2);
-
-	return 0;
+	scanf_s("%d %d", &H, &M);
+	early(H, M);
 }
 
-int calculator(int num1, char cal, int num2)
+void early(int H, int M)
 {
-	int result;
-
-	switch (cal)
-	{
-	case '+':
-		result = num1 + num2;
-		printf("%d%c%d = %d\n", num1, cal, num2, result);
-		break;
-	case '-':
-		result = num1 - num2;
-		printf("%d%c%d = %d\n", num1, cal, num2, result);
-		break;
-	case '*':
-		result = num1 * num2;
-		printf("%d%c%d = %d\n", num1, cal, num2, result);
-		break;
-	case '/':
-		result = num1 / num2;
-		printf("%d%c%d = %d\n", num1, cal, num2, result);
-		break;
-	default:
-		printf("연산식을 찾을 수 없습니다.\n");
-		break;
-	}
-
-return result;
+	if (H != 0)
+		if (M >= 45)
+			printf("%d %d", H, (M - 45));
+		else
+			printf("%d %d", (H - 1), (M + 60 - 45));
+	else
+		if (M >= 45)
+			printf("%d %d", (H), (M - 45));
+		else
+			printf("%d %d", (H + 24 - 1), (M + 60 - 45));
 }
-		
 
+
+//사분면 고르기
+
+void main(void)
+{
+	int x, y;
+
+	scanf_s("%d", &x);
+	scanf_s("%d", &y);
+
+	if ((x != 0) && (y != 0))
+		if (x > 0)
+			if (y > 0)
+				printf("1");
+			else
+				printf("4");
+		else
+			if (y > 0)
+				printf("2");
+			else
+				printf("3");
+	else
+		printf("사분면에 속하지 않습니다.\n");
+}
+
+
+//윤년
+
+void main(void)
+{
+	int year;
+
+	scanf_s("%d", &year);
+
+	if ((year % 4) == 0)
+		if ((year % 100) == 0)
+			if ((year % 400) == 0)
+				printf("1");
+			else
+				printf("0");
+		else
+			printf("1");
+	else
+		printf("0");
+
+}
