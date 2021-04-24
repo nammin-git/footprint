1857f82d2af8112fa2786c55a2356c64a484e955
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-24 23:15:46 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,92 +1,84 @@
 #include <stdio.h>
 
-void early(int H, int M);
-
 void main(void)
 {
-	//알람 시계 1
-	int H, M;
+	//Programming 문제
+	//1. 자음 모음 구별 프로그램
+	char ch;
 
-	scanf_s("%d %d", &H, &M);
+	printf("문자를 입력하시오: ");
+	scanf_s("%c", &ch, 1);
 
-	//if ((M >= 0) || (M <= 59))
-	//	if ((H >= 0) || (H <= 23))
-	if (H != 0)
-		if (M >= 45)
-			printf("%d %d", H, (M - 45));
-		else
-			printf("%d %d", (H - 1), (M + 60 - 45));
+	switch(ch)
+	{
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			printf("모음입니다.\n");
+			break;
+		default:
+			printf("자음입니다.\n");
+			break;
+	}
+
+
+	//2. 첫 번째 수가 두 번째 수의 약수인지 알려주는 프로그램
+	int num1, num2;
+
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &num1);
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &num2);
+
+	if ((num1 % num2) == 0)
+		printf("약수입니다.\n");
 	else
-		if (M >= 45)
-			printf("%d %d", (H), (M - 45));
-		else
-			printf("%d %d", (H + 24 - 1), (M + 60 - 45));
+		printf("약수가 아닙니다.\n");
 
 
-	//알람 시계 2
-	int H, M;
+	//3. 가장 작은 값을 찾아내는 프로그램
+	int num1, num2, num3;
 
-	scanf_s("%d %d", &H, &M);
-	early(H, M);
-}
-
-void early(int H, int M)
-{
-	if (H != 0)
-		if (M >= 45)
-			printf("%d %d", H, (M - 45));
-		else
-			printf("%d %d", (H - 1), (M + 60 - 45));
+	printf("세 개의 정수를 입력하시오: ");
+	scanf_s("%d %d %d", &num1, &num2, &num3);
+	
+	//else if 사용
+	if ((num1 < num2) && (num1 < num3))
+		printf("제일 작은 정수는 %d입니다.", num1);
+	else if ((num2 < num1) && (num2 < num3))
+		printf("제일 작은 정수는 %d입니다.", num2);
 	else
-		if (M >= 45)
-			printf("%d %d", (H), (M - 45));
+		printf("제일 작은 정수는 %d입니다.", num3);
+
+	//if 와 else 만 사용
+	if (num1 < num2)
+		if (num1 < num3)
+			printf("제일 작은 정수는 %d입니다.", num1);
 		else
-			printf("%d %d", (H + 24 - 1), (M + 60 - 45));
-}
-
-
-//사분면 고르기
-
-void main(void)
-{
-	int x, y;
-
-	scanf_s("%d", &x);
-	scanf_s("%d", &y);
-
-	if ((x != 0) && (y != 0))
-		if (x > 0)
-			if (y > 0)
-				printf("1");
-			else
-				printf("4");
-		else
-			if (y > 0)
-				printf("2");
-			else
-				printf("3");
+			printf("제일 작은 정수는 %d입니다.", num3);
 	else
-		printf("사분면에 속하지 않습니다.\n");
-}
-
-
-//윤년
-
-void main(void)
-{
-	int year;
-
-	scanf_s("%d", &year);
-
-	if ((year % 4) == 0)
-		if ((year % 100) == 0)
-			if ((year % 400) == 0)
-				printf("1");
-			else
-				printf("0");
+		if (num2 < num3)
+			printf("제일 작은 정수는 %d입니다.", num2);
 		else
-			printf("1");
-	else
-		printf("0");
+			printf("제일 작은 정수는 %d입니다.", num3);
 
+
+	//4. 가위바위보 게임
+	?
+
+
+	//5. 롤러코스터에 타도 됩니까?
+	int height, age;
+
+	printf("키를 입력하시오(cm): ");
+	scanf_s("%d", &height);
+	printf("나이를 입력하시오: ");
+	scanf_s("%d", &age);
+
+	if ((height >= 140) && (age >= 10))
+		printf("타도 좋습니다.");
+	else
+		printf("탈 수 없습니다.");
 }
