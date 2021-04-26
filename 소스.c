fa7e11f5f221fa63e644fc5e0bbdf3c49fc1b0d8
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-26 22:08:58 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -2,83 +2,54 @@
 
 void main(void)
 {
-	//Programming 문제
-	//1. 자음 모음 구별 프로그램
-	char ch;
+	//1. 구구단 만들기
+	int N;
+	int i;
 
-	printf("문자를 입력하시오: ");
-	scanf_s("%c", &ch, 1);
+	scanf_s("%d", &N);
 
-	switch(ch)
+	for (i = 1; i < 10; i++)
 	{
-		case 'a':
-		case 'e':
-		case 'i':
-		case 'o':
-		case 'u':
-			printf("모음입니다.\n");
-			break;
-		default:
-			printf("자음입니다.\n");
-			break;
+		printf("%d * %d = %d\n", N, i, N * i);
 	}
 
 
-	//2. 첫 번째 수가 두 번째 수의 약수인지 알려주는 프로그램
-	int num1, num2;
+	//3. 1부터 n까지 합 구하기
+	int n, i, sum = 0;
 
-	printf("정수를 입력하시오: ");
-	scanf_s("%d", &num1);
-	printf("정수를 입력하시오: ");
-	scanf_s("%d", &num2);
+	scanf_s("%d", &n);
 
-	if ((num1 % num2) == 0)
-		printf("약수입니다.\n");
-	else
-		printf("약수가 아닙니다.\n");
+	if ((n >= 1) && (n < 10000))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
 
 
-	//3. 가장 작은 값을 찾아내는 프로그램
-	int num1, num2, num3;
+	//5. 1부터 N까지 한 줄에 하나씩 출력하기
+	int N, i;
 
-	printf("세 개의 정수를 입력하시오: ");
-	scanf_s("%d %d %d", &num1, &num2, &num3);
+	scanf_s("%d", &N);
 	
-	//else if 사용
-	if ((num1 < num2) && (num1 < num3))
-		printf("제일 작은 정수는 %d입니다.", num1);
-	else if ((num2 < num1) && (num2 < num3))
-		printf("제일 작은 정수는 %d입니다.", num2);
-	else
-		printf("제일 작은 정수는 %d입니다.", num3);
+	if ((N>0)&&(N<100000)){
+		for (i = 1; i <= N; i++) {
+			printf("%d\n", i);
+		} 
+	}
+	
 
-	//if 와 else 만 사용
-	if (num1 < num2)
-		if (num1 < num3)
-			printf("제일 작은 정수는 %d입니다.", num1);
-		else
-			printf("제일 작은 정수는 %d입니다.", num3);
-	else
-		if (num2 < num3)
-			printf("제일 작은 정수는 %d입니다.", num2);
-		else
-			printf("제일 작은 정수는 %d입니다.", num3);
+	//6. N부터 1까지 한 줄에 하나씩 출력하기
+	int N, i;
 
+	scanf_s("%d", &N);
 
-	//4. 가위바위보 게임
-	?
-
-
-	//5. 롤러코스터에 타도 됩니까?
-	int height, age;
-
-	printf("키를 입력하시오(cm): ");
-	scanf_s("%d", &height);
-	printf("나이를 입력하시오: ");
-	scanf_s("%d", &age);
-
-	if ((height >= 140) && (age >= 10))
-		printf("타도 좋습니다.");
-	else
-		printf("탈 수 없습니다.");
+	if ((N > 0) && (N < 100000)) {
+		for (i = N; i >= 1; i--) {
+			printf("%d\n", i);
+		}
+	}
 }
