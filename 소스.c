cef6106add47ba779914f4e10a213e9c9dc6c870
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-04-27 23:53:41 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -2,54 +2,117 @@
 
 void main(void)
 {
-	//1. 구구단 만들기
-	int N;
-	int i;
+	//11. X보다 작은 수 출력하기
+	int N, X, x, i;
 
-	scanf_s("%d", &N);
+	scanf_s("%d %d", &N, &X);
 
-	for (i = 1; i < 10; i++)
+	if (((N >= 1) && (N <= 10000)) && ((X >= 1) && (X <= 10000)))
 	{
-		printf("%d * %d = %d\n", N, i, N * i);
+		for (i = 1; i <= N; i++)
+		{
+			scanf_s("%d", &x);
+
+			if (X > x)
+				printf("%d ", x);
+		}
+	}
+	//어떻게 된 거지...?
+
+
+	//for문을 이용하여 meter를 mile로 바꾸기
+	int meter, mile;
+
+	for (mile = 0; mile <= 2; mile++)
+	{
+		meter = 1609 * mile;
+		printf("%d마일은 %d미터입니다.\n", mile, meter);
+	}
+
+	//while문을 이용하여 meter를 mile로 바꾸기
+	int i = 0;
+
+	while (i < 3)
+	{
+		meter = i * 1609;
+		printf("%d마일은 %d미터입니다.\n", i, meter);
+		i++;
 	}
 
 
-	//3. 1부터 n까지 합 구하기
-	int n, i, sum = 0;
+	//while문을 이용하여 구구단 출력하기
+	int n;
+	int i = 1;
 
+	printf("출력하고 싶은 단: ");
 	scanf_s("%d", &n);
 
-	if ((n >= 1) && (n < 10000))
+	while (i <= 9)
 	{
-		for (i = 0; i <= n; i++)
-		{
+		printf("%d * %d = %d\n", n, i, n * i);
+		i++;
+	}
+
+
+	//while문을 이용한 제곱값 출력 프로그램
+	int n;
+	
+	printf("==============================\n");
+	printf("    n             n의 제곱\n");
+	printf("==============================\n");
+
+	n = 1;
+	while (n <= 10)
+	{
+		printf("%5d          %5d\n", n, n * n);
+		n++;
+	}
+
+
+	//while문을 사용하여 1부터 n까지의 합 계산하기
+	int n, i, sum;
+
+	printf("정수를 입력하시요: ");
+	scanf_s("%d", &n);
+
+	i = 1;
+	sum = 0;
+	while (i <= n)
+	{
+		sum += i;
+		i++;
+	}
+
+	printf("1부터 %d까지의 합은 %d입니다.\n", n, sum);
+
+
+	//while문을 이용하여 1~n 사이의 짝수들의 합 구하기
+	int n, sum, i;
+
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &n);
+
+
+	i = 0;
+	sum = 0;
+	while (i <= n)
+	{
+		sum += i;
+		i = i + 2;
+	}
+
+	printf("1부터 %d까지의 짝수들의 합은 %d입니다.\n", n, sum);
+
+	//나는 if문을 사용함
+	i = 1;
+	sum = 0;
+	while (i <= n)
+	{
+		if (i % 2 == 0)
 			sum += i;
-		}
+		i++;
 	}
 
-	printf("%d\n", sum);
+	printf("1부터 %d까지의 짝수들의 합은 %d입니다.\n", n, sum);
 
-
-	//5. 1부터 N까지 한 줄에 하나씩 출력하기
-	int N, i;
-
-	scanf_s("%d", &N);
-	
-	if ((N>0)&&(N<100000)){
-		for (i = 1; i <= N; i++) {
-			printf("%d\n", i);
-		} 
-	}
-	
-
-	//6. N부터 1까지 한 줄에 하나씩 출력하기
-	int N, i;
-
-	scanf_s("%d", &N);
-
-	if ((N > 0) && (N < 100000)) {
-		for (i = N; i >= 1; i--) {
-			printf("%d\n", i);
-		}
-	}
 }
