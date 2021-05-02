61f470984c8ffbdb675afe206f5744d247a2c8ad
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-05-02 13:56:40 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,66 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 void main(void)
 {
-	//가위바위보 게임
-	int a;
-	int b = (rand() % 100);
+	//별로 사각형 만들기
+	int side, i;
+	int j;
 
-	printf("선택하시오(1: 가위 2: 바위 3: 보) ");
-	scanf_s("%d", &a);
+	printf("몇 개의 별로 한 변을 만들지 입력하시오: ");
+	scanf_s("%d", &side);
 
-	if (a == b)
-		printf("비겼음\n");
-	else if (a - b == 1)
-		printf("사용자가 이김\n");
-	else if (a - b == -1)
-		printf("컴퓨터가 이김\n");
-	else if (a - b == 2)
-		printf("컴퓨터가 이김\n");
-	else if (a - b == -2)
-		printf("사용자가 이김\n");
-	else
-		printf("알맞은 숫자를 입력하시오.\n");
-	exit(0);
-	//****************난수가 발생이 안 되어 실패
+	//속이 빈 사각형	
+	
+	printf("**************\n");
+	for (i = 0; i < side; i++)
+		printf("*            *\n");
+	printf("**************\n");
 
-
-	//for문으로 Hello World 찍어내기
-	int i;
-
-	for (i = 0; i < 10; i++)
-		printf("Hello World\n");
-
-
-	//for문을 사용하여 1부터 10까지의 정수를 더하기
-	int i, sum = 0;
-
-	for (i = 1; i <= 10; i++) {
-		sum += i;
+	//속이 찬 사각형              
+	for (i = 0; i < side; i++)
+	{
+		printf("*");
+		for (j = 0; j < side; j++)
+			printf("\n*");
 	}
-
-	printf("1부터 10까지의 총합은 %d입니다.\n", sum);
-
-
-	//for문을 사용하여 세제곱을 나열하는 프로그램
-	int n, i, pn;
-
-	printf("정수를 입력하시오: ");
-	scanf_s("%d", &n);
-
-	printf("==================================\n");
-	printf("     n              n의 세제곱       \n");
-	printf("==================================\n");
-
-	for (i = 1; i <= n; i++) {
-		pn = i * i * i;
-		printf("%6d", i);
-		printf("%20d\n", pn);
-	}
-	//변수 최소화
-	for (i = 1; i <= n; i++) {
-		printf("%6d              %6d\n", i, i * i * i);
-	}
-
+	//실패
 }
