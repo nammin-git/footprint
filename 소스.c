f600388194eb55b2f1912de2795fa42abed7ef9b
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-05-06 22:48:55 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,70 +1,59 @@
 #include <stdio.h>
+#define START_DAY 3
+#define DAYS_OF_MONTH 31
+
 
 void main(void)
 {
-	//소문자를 대문자로 변경하는 프로그램
-	char letter;
-	
-	while (1) {
-		printf("소문자를 입력하세요: ");
-		scanf_s(" %c", &letter, 1); //공백문자 제외
-		
-		if (letter == 'Q')
-			break;
-		else if (letter < 'a' || letter>'z')
-			continue;
+	//복리 이자 계산
+	double seed_money, interest_rate, total;
+	int year, i = 0;
 
-		letter -= 32;
-		printf("변환된 대문자는 %c입니다.\n", letter);
-	}
-	
+	printf("원금: ");
+	scanf_s("%lf", &seed_money);
+	printf("이율(%%): ");
+	scanf_s("%lf", &interest_rate);
+	printf("기간(년): ");
+	scanf_s("%d", &year);
 
-	//3의 배수를 걸러내는 프로그램
-	int i;
+	printf("==================\n");
+	printf("연도       원리금\n");
+	printf("==================\n");
 
-	for (i = 1; i < 10; i++) {
-		if (i % 3 == 0)
-			//break;
-			continue;
-		printf("%d ", i);
+	total =	seed_money;
+	interest_rate = interest_rate / 100;
+
+	while (i < year) {
+		total *= (1 + interest_rate);
+		printf("%d         %.1f\n", i+1, total);
+		i++;
 	}
 
 
-	//파이 구하기
-	int n, i;
-	double pi = 0.0;
-	double k = 1.0;
+	//달력 출력하기
+	int day, date;
 
-	printf("반복 횟수를 입력하시오: ");
-	scanf_s("%d", &n);
+	printf("=============================\n");
+	printf(" 일  월  화  수  목  금  토\n");
+	printf("=============================\n");
 
-	for (i = 0; i < n; i++) {
-		pi += 4.0 / k - 4.0 / (k + 2.0);
-		k = k + 4.0;
-	}
+	for (day = 0; day < START_DAY; day++)
+		printf("    ");
 
-	printf("Pi = %f", pi);
-	//맞나? 숫자 다름
-
-	
-	//파이 구하기 2
-	int loop_count;
-	double pi = 3.0;
-	double k = 2.0;
-
-	printf("반복 횟수를 입력하시오: ");
-	scanf_s("%d", &loop_count);
-
-	while (loop_count > 0) {
-		if(loop_count%2==1)
-			pi += 4.0 / (k * (k + 1.0) * (k + 2.0));
+	for (date = 1; date <= DAYS_OF_MONTH; date++) {
+		if (day == 7) {
+			day = 0;
+			printf("\n");
+		}
+		day++;
+		if (day == 0)
+			printf("%d ", date);
 		else
-			pi -= 4.0 / (k * (k + 1.0) * (k + 2.0));
-		k = k + 2.0;
-		loop_count--;
+			printf("%3d ", date);
 	}
 
-	printf("pi = %.20f\n", pi);
+	printf("\n=============================\n");
+	//다시 해야함
 
 
 }
