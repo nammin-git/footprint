f7243cbb0a661e1ebfe8f83ec2f02fbe1cb0feb2
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-05-10 22:04:08 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,83 +1,125 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 void main(void)
 {
 	//반복문 문제 3
 	//나이에 따라 지원금 지급하기
-	int age, i = 0;
-	int elderly = 0, adult = 0, juvenile = 0, kid = 0;
+	int age = 1;
+	int sinior = 0, adult = 0, teenager = 0, junior = 0;
 
 	while (1) {
-		printf("가족 구성원의 나이를 입력해주세요 (0 입력 시 종료): ");
-		scanf_s("%d", &age);
-
-		if (age==0)
+		if (age != 0) {
+			printf("가족 구성원의 나이를 입력해주세요(0 입력시 종료): ");
+			scanf_s("%d", &age);
+		}
+		else
 			break;
-
 		if ((age >= 1) && (age <= 12))
-			kid++;
-		else if ((age >= 13) && (age <= 19))
-			juvenile++;
+			junior++;
+		else if ((age >= 12) && (age <= 19))
+			teenager++;
 		else if ((age >= 20) && (age <= 64))
 			adult++;
-		else if ((age >= 65))
-			elderly++;
-
-		i++;
+		else if (age >= 65)
+			sinior++;
 	}
+	
+	printf("..조사 종료..\n총 %d명 노인 %d명, 성인 %d명, 청소년 %d명, 아동 %d명\n", sinior+adult+teenager+junior, sinior, adult, teenager, junior);
+	printf("코로나 지원금은 총 %d만원 입니다.\n", 10 * sinior + 20 * (teenager + junior));
 
-	printf("..조사종료..\n<귀댁의 가족 구성>\n");
-	printf("총 %d명 노인 %d명, 성인 %d명, 청소년 %d명, 아동 %d명\n", i, elderly, adult, juvenile, kid);
-	printf("코로나 지원금은 총 %d만원 입니다.\n", 10*elderly+20*(juvenile+kid));
 
+	//이중 반복문 문제 3 - 1
+	//입력받은 수를 가지고 별 출력하기
+	int N, x, y;
 
-	//이중 반복문 문제 1-1
-	//별로 탑 쌓기
-	int x, y;
+	printf("N을 입력하시오: ");
+	scanf_s("%d", &N);
 
-	for (y = 1; y <= 5; y++) {
-		for (x = 1; x <= y; x++) {
+	for (y = 0; y < N; y++) {
+		for (x = 0; x <= y; x++)
 			printf("*");
-		}
-		printf("\n");	
+		printf("\n");
+	}
+
+	//이중 반복문 문제 3 - 2
+	//입력받은 수를 가지고 별 출력하기
+	int N, x, y;
+
+	printf("N을 입력하시오: ");
+	scanf_s("%d", &N);
+
+	for (y = N; y > 0; y--) {
+		for (x = 1; x <= y; x++)
+			printf("*");
+		printf("\n");
 	}
 
 
-	//이중 반복문 문제 1-2
-	//별로 거꾸로된 탑 쌓기
+	//이중 반복문 문제 1 - 2
+	//별탑 반대로 쌓기
 	int x, y;
 
 	for (y = 5; y > 0; y--) {
-		for (x = 5; x >= y; x--)
+		for (x = 1; x <= y; x++)
 			printf("*");
 		printf("\n");
 	}
-	//안 됨
 
 
-	//이중 반복문 문제 2-1
-	//구구단 출력 가로버전
-	int table, n;
+	//이중 반복문 문제 4 - 1
+	//별 탑 오른쪽 정렬
+	int x, y, z;
 
-	for (table = 1; table <= 9; table++) {
-		for (n = 1; n <= 9; n++) {
-			printf("%dx%d=%d ", table, n, table * n);
+	for (y = 0; y < 5; y++) {
+		for (z = y; 5 - z > 0; z++)
+				printf(" ");
+		for (x = 0; x <= y; x++)
+			printf("*");
+						
+		printf("\n");
+	}
+
+
+	//이중 반복문 문제 4 - 1
+	//별 탑 오른쪽 정렬
+	int x, y, z;
+
+	for (y = 0; y < 5; y++) {
+		for (x = 0; x <= y; x++)
+			printf(" ");
+		for (z = y; 5 - z >0 ; z++)
+			printf("*");
+						
+		printf("\n");
+	}
+
+
+	//이중 반복문 문제 5
+	//별 기호로 마름모 모양 만들기
+	int x, y, z;
+
+	for (y = 1; y <= 9; y++) {
+		if (y < 5) {
+			for (z = y; 5 - z > 0; z++)
+				printf(" ");
+			for (x = 1; x <= 2*y-1; x++)
+				printf("*");
+			printf("\n");
 		}
-		printf("\n");
+		else if (y == 5) {
+			for (x = 1; x <= 2 * y - 1; x++)
+				printf("*");
+			printf("\n");
+		}
+		else 
+		{
+			for (z = 10 - y; 5 - z > 0; z++)
+				printf(" ");
+			for (x = 1; x <= 19 - 2 * y; x++)
+				printf("*");
+			printf("\n");
+		}
+		//왜 19? 방정식으로 해보니까 식이 나오던데 왜 19인지 모름
 	}
-
-	//이중 반복문 문제 2-2
-	//구구단 출력 세로버전
-	int table, n;
-	for (n = 1; n <= 9; n++) {
-		for (table = 1; table <= 9; table++)
-			if (table*n<10)
-				printf("%dX%d=%d  ", table, n, table * n);
-			else
-				printf("%dX%d=%d ", table, n, table * n);
-		printf("\n");
-	}
-
 
 }
