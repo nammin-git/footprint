f9e1e97375ecd67d60434f9d308005eb0bcd080d
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-05-12 23:56:45 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,79 +1,52 @@
+//2021 달력 만들기
+
 #include <stdio.h>
-#include <stdlib.h>
+#define YEAR 2021
+#define FIRST_DAY_OF_YEAR 5
+
+int how_many_date(int month);
 
 void main(void)
 {
-	//반복문 문제 3
-	//나이에 따라 지원금 지급하기
-	int age;
-	int elderly = 0, adult = 0, teenager = 0, kid = 0;
+	int day, date, month = 1;
+	printf("===========================\n");
+	printf("%20d년 %d월\n", YEAR, month);
 
-	do {
-		printf("가족 구성원의 나이를 입력해주세요(0 입력 시 종료): ");
-		scanf_s("%d", &age);
-		if ((age >= 1) && (age <= 12))
-			kid++;
-		else if ((age >= 13) && (age <= 19))
-			teenager++;
-		else if ((age >= 20) && (age <= 64))
-			adult++;
-		else if (age >= 65)
-			elderly++;
-	} while (age != 0);
+	printf("---------------------------\n");
+	printf("일  월  화  수  목  금  토\n");
+
+	printf("---------------------------\n");
+	for (day = 0; day < FIRST_DAY_OF_YEAR; day++)
+		printf("  ");
+	for (date = 1; date <= how_many_date(month); date++);
+	
+	printf("---------------------------\n");
 
 
-	//do...while
-	int i = 10;
-	do {
-		printf("i = %d\n", i);
-		i++;
-	} while (i < 3);
+	printf("===========================\n");
 
 
-	//do...while을 이용한 메뉴
-	int i = 0;
 
-	do {
-		printf("1---새로만들기\n");
-		printf("2---파일열기\n");
-		printf("3---파일닫기\n\n");
-		printf("하나를 선택하시오: ");
-		scanf_s("%d", &i);
-	} while (i < 1 || i>3);
+}
 
-	printf("\n\n");
+int how_many_date(int month)
+{
+	int date = 0;
 
-	switch (i) {
-	case 1:
-		printf("1을 선택하셨습니다.\n새로만들기를 실행합니다.\n");
-		break;
+	switch (month) {
 	case 2:
-		printf("2를 선택하셨습니다.\n파일열기를 실행합니다.\n");
+		date = 28;
 		break;
-	case 3:
-		printf("3을 선택하셨습니다.\n파일닫기를 실행합니다.\n");
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		date = 28;
+		break;
+	default:
+		date = 31;
 		break;
 	}
 
-
-	//숫자 추측 게임
-	int answer = 59;
-	int guess;
-	int tries = 0;
-
-	do {
-		printf("정답을 추측하여 보시오: ");
-		scanf_s("%d", &guess);
-		tries++;
-
-		if (guess > answer)
-			printf("제시한 정수가 높습니다.\n");
-		if (guess < answer)
-			printf("제시한 정수가 낮습니다.\n");
-	} while (guess != answer);
-
-	printf("축하합니다. 정답입니다.\n시도횟수 %d번\n", tries);
-
-
-
+	return date;
 }
