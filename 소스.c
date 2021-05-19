f6e6cab62adbc73bd9b9eff7f85b5c2c6e755c7a
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-05-19 15:53:25 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void draw_star(int N);
 void draw_star_ri(int N);
 
 void main(void)
-{/*
+{
 	//이중 반복문 문제 3
 	//*
 	//**
@@ -27,17 +29,50 @@ void main(void)
 	scanf_s("%d", &N);
 
 	draw_star_ri(N);
-*/
+
 	
 	//이중 반복문 문제 5
 	//마름모 모양 만들기
 	int x, y;
 
 	for (y = 1; y <= 9; y++) {
-		if(y<=5)
-			for(x=1; x<=9; x+=2)
-				if
+		for (x = 1; x <= 9; x++) {			
+			if ((y >= 1) && (y <= 5) && (x >= 1) && (x <= 5)) {
+				if (x > 5 - y)
+					printf("*");
+				else
+					printf(" ");
+			}
+			else if ((y >= 1) && (y <= 5) && (x > 5) && (x <= 9)) {
+				if (x - 4 <= y)
+					printf("*");
+				else
+					printf(" ");
+			}
+			else if ((y > 5) && (y <= 9) && (x >= 1) && (x <= 5)) {
+				if (x + 4 >= y)
+					printf("*");
+				else
+					printf(" ");
+			}
+			else if ((y > 5) && (y <= 9) && (x > 5) && (x <= 9)) {
+				if (x <= 14 - y)
+					printf("*");
+				else
+					printf(" ");
+			}
+		}
+		printf("\n");
 	}
+
+	 
+	//난수 발생 함수
+	int i;
+
+	srand((unsigned)time(NULL));
+
+	for (i = 0; i < 6; i++)
+		printf("%d ", 1 + rand() % 45);
 
 }
 
