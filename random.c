65527db36a73cb887839744a57823499e1d2f10f
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-05-21 20:39:15 +0900

    Add files via upload

---
+++ random.c
@@ -0,0 +1,24 @@
+#define SEED 17
+#define MULT 25173
+#define INC 13849
+#define MOD 65536
+
+unsigned int call_count = 0;
+static unsigned seed = SEED;
+
+unsigned random_i() {
+	seed = (MULT * seed + INC) % MOD;
+	call_count++;
+
+	return seed;
+}
+
+double random_f() {
+	seed = (MULT * seed + INC) % MOD;
+	call_count++;
+
+	return seed / (double)MOD;
+}
+
+
+
--- 소스.c
+++ 소스.c
@@ -1,134 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MY_ID 5678
-#define MY_PW 7890
+unsigned random_i();
+double random_f();
 
-#define SUCCESS 1
-#define FAIL 2
-#define LIMIT 3
+long factorial(int n);
 
-int check(int id, int password);
+extern unsigned call_count;
 
-void sub();
-
-int main(void)
+void main()
 {
-	//범위 - 지역 변수
-	int i = 0;
-	
-	for (i = 0; i < 5; i++) {
-		int temp = 1;
-		printf("temp = %d\n", temp);
-		temp++; //중괄호는 블록 -> 지역 변수 temp는 매번 새로 시작
-	}
-
-	printf("\n===============\n");
-
-	while (i < 10) {
-		int temp = 0;
-		printf("temp = %d\n", temp);
-		temp++; //마찬가지
-		i++;
-	}
-
-
-	//범위 - 전역 변수
-
-
-	//생존 시간 - auto
-
-
-	//생존 시간 - static
-	int i;
-
-	for (i = 0; i < 3; i++) {
-		sub();
-	}
-
-
-	//생존 시간 - register
+	//난수를 생성하는 프로그램
+	//random_i 함수
 	register int i;
-	register int sum = 0;
 
-	for (i = 0; i < 100; i++) {
-		sum += i;
-		printf("%d\n", sum);
+	for (i = 0; i < 10; i++)
+		printf("%d ", random_i());
+
+	printf("\n");
+
+	for (i = 0; i < 10; i++) {
+		printf("%f ", random_f());
 	}
 
-
-	//생존 시간 - extern
-
-
-	//로그인 횟수 제한하기
-	//first try
-	int count = 0;
-
-	while (count < 3) {
-		static int id, pw;
-		printf("id: ");
-		scanf_s("%d", &id);
-		printf("pass: ");
-		scanf_s("%d", &pw);
-
-		if ((id == MY_ID) && (pw == MY_PW)) {
-			printf("로그인 성공\n");
-			break;
-		}
-
-		count++;
-	}
-	
-	if (count == 3)
-		printf("횟수 초과\n");
+	printf("\n함수가 호출된 횟수= %d \n", call_count);
 
 
-	//로그인 횟수 제한하기
-	//second try
-	int id, password, result;
+	//순환
+	//팩토리얼 계산 프로그램
+	int n;
 
-	while (1) {
-		printf("id: ____\b\b\b\b");
-		scanf_s("%d", &id);
-		printf("pw: ____\b\b\b\b");
-		scanf_s("%d", &password);
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &n);
 
-		result = check(id, password);
-
-		if (result == SUCCESS)
-			break;
-		else
-			printf("다시 시도해주세요.\n\n");
-	}
-
-	printf("로그인 성공\n");
+	printf("%d!은 %d입니다.", n, factorial(n));
 }
 
-void sub() {
-	int auto_count = 0;
-	static int static_count = 0;
+long factorial(int n) {
+	printf("factorial(%d)\n", n);
 
-	auto_count++;
-	static_count++;
-
-	printf("auto_count=%d\n", auto_count);
-	printf("static_count=%d\n\n", static_count);
-}
-
-int check(int id, int password) {
-	static int super_id = 1234;
-	static int super_password = 5678;
-	static int try_count = 0;
-
-	try_count++;
-
-	if (try_count >= LIMIT) {
-		printf("횟수 초과\n");
-		exit(1);
-	}
-	
-	if ((id == super_id) && (password == super_password))
-		return SUCCESS;
+	if (n <= 1)
+		return 1;
 	else
-		return FAIL;
+		return n * factorial(n - 1);
 }
