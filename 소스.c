acb0a55a6e07a685b71d5c18e96442de42866d31
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-05-23 00:13:37 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -4,6 +4,8 @@ long long sum(int* a, int n);//안 배운 거...
 
 long factorial(int N);
 
+long Fibonacci(int Fn);
+
 void main(void)
 {
 	//정수 n개의 합
@@ -13,17 +15,43 @@ void main(void)
 	//팩토리얼
 	int N;
 
+	while (1) {
 	printf("정수를 입력하시오: ");
 	scanf_s("%d", &N);
 
-	printf("N! = %d", factorial(N));
+	if ((N >= 0) && (N <= 12)) {
+		printf("N! = %d\n", factorial(N));
+		break;
+	}
+	else
+		printf("0부터 12 사이의 정수를 입력해야 합니다.\n");
+	}
 
+
+
+	//피보나치 수
+	int n;
+
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &n);
+
+	printf("%d번째 피보나치 수는 %d입니다.\n", n, Fibonacci(n));
 
 }
 
 long factorial(int N) {
-	if (N == 1)
+	if (N <= 1)
 		return 1;
 	else
-		return factorial(N - 1);
+		return N * factorial(N - 1);
+}
+
+long Fibonacci(int Fn) {
+	if (Fn == 0)
+		return 0;
+	else if (Fn == 1)
+		return 1;
+	else
+		return Fibonacci(Fn - 1) + Fibonacci(Fn - 2);
+
 }
