53d685234bb4ceaee70bcb2b69b1c76f89d30b74
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-05-22 23:57:09 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,45 +1,29 @@
 #include <stdio.h>
 
-unsigned random_i();
-double random_f();
+long long sum(int* a, int n);//안 배운 거...
 
-long factorial(int n);
+long factorial(int N);
 
-extern unsigned call_count;
-
-void main()
+void main(void)
 {
-	//난수를 생성하는 프로그램
-	//random_i 함수
-	register int i;
-
-	for (i = 0; i < 10; i++)
-		printf("%d ", random_i());
-
-	printf("\n");
-
-	for (i = 0; i < 10; i++) {
-		printf("%f ", random_f());
-	}
-
-	printf("\n함수가 호출된 횟수= %d \n", call_count);
+	//정수 n개의 합
+	//패쓰
 
 
-	//순환
-	//팩토리얼 계산 프로그램
-	int n;
+	//팩토리얼
+	int N;
 
 	printf("정수를 입력하시오: ");
-	scanf_s("%d", &n);
+	scanf_s("%d", &N);
 
-	printf("%d!은 %d입니다.", n, factorial(n));
+	printf("%d", factorial(N));
+
+
 }
 
-long factorial(int n) {
-	printf("factorial(%d)\n", n);
-
-	if (n <= 1)
+long factorial(int N) {
+	if (N == 1)
 		return 1;
 	else
-		return n * factorial(n - 1);
+		return fatorial(N - 1);
 }
