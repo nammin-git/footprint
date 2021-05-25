58960bda9d68fcbf2995584366514f389e75b595
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-05-25 19:53:17 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,58 +1,18 @@
 #include <stdio.h>
 
-void draw_star(int N);
-int draw_star(int N);
-
-int i = 1;
+double get_pc_temp();
 
 void main(void)
 {
-	//재귀적인 패턴으로 별 찍기
-	int N;
+	double temperature;
+	temperature = get_pc_temp();
 
-	printf("3의 거듭제곱인 정수를 입력하시오: ");
-	scanf_s("%d", &N);
-
-
-
-	while (N / 3 > 1) {
-		N = N / 3;
-		i++;
-	}
+	if (temperature >= 50)
+		printf("과열되었습니다.\n");
 	
-
-
-	//하노이 탑 이동 순서
-	int n;
-
-	printf("원판의 갯수를 입력하시오: ");
-	scanf_s("%d", &n);
-
-
 }
 
-void draw_star(int N) {
-	int i, j;
-
-	for (i = 1; i <= N; i++) {
-		for (j = 1; j <= N; j++) {
-			if ((i == N / 3 + 1) && (j == N / 3 + 1))
-				printf(" ");
-			else
-				printf("*");
-		}
-		printf("\n");
-	}
+double get_pc_temp() {
+	printf("get_pc_temp()가 호출되었습니다.\n");
+	return 49.0;
 }
-
-
-int draw_star(int N) {
-	if (N / 3 > 1)
-		return draw_star(N / 3);
-	else
-		return 
-
-}
-
-
-
