5b450e7b9d84ba815b9475b480e8c3fa8ac19ce2
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-06-08 21:10:59 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -4,30 +4,31 @@
 void main()
 {
 	int a, b, c;
-	printf("자연수를 입력하시오: ");
+	unsigned int result, divisor;
+
 	scanf_s("%d", &a);
 	scanf_s("%d", &b);
 	scanf_s("%d", &c);
-	
-	printf("\n-----------------\n\n");
+
+	result = a * b * c;
+	divisor = 1000000000;
+
+	printf("%d\n", result);
 
 	int num[10] = { 0 };
-	unsigned long result;
-	result = a * b * c;
-	unsigned long divisor = 100000000;
-	
-	while (result<10) {
-		if (result / divisor == 0)
-			divisor /= 10;
-		else {
-			num[result / divisor + 1] = result / divisor;
-			result = result % divisor;
-			divisor /= 10;
-		}
+
+	while (result/divisor!=0) {
+		divisor /= 10;
 	}
 
-	for (int i = 0; i < 10; i++) {
-		printf("%d\n", num[i]);
+	while (result > 10) {
+		num[result / divisor]++;
+		result %= divisor;
+		divisor /= 10;
 	}
+
+	for (int i = 0; i < 10; i++)
+		printf("%d\n", num[i]);
+
 
 }
