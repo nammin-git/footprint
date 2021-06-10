36e217b33882d0c128f19d6b711bffbd0157d70d
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-06-10 22:24:55 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,127 +1,41 @@
-//반복문 문제
+//랜덤함수-배열-검색
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-//2번
 void main()
 {
-	int n = 1;
-	int sum = 0;
-	int result;
+	int arr[10];
 
-	while (n < 100) {
-		if (n == 1) {
-			result = n;
-			sum = result;
-			printf("%d + ", n);
-		}
-		else if (n < 97) {
-			result += (n - 2) + n;
-			sum += result;
-			printf("%d + ", result);
-		}
-		else {
-			result += (n - 2) + n;
-			sum += result;
-			printf("%d ", result);
-		}
-		n += 4;
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i < 10; i++) {
+		arr[i] = rand() % 50 + 1;
 	}
 
-	printf("= %d", sum);
-}
+	int target, count=0;
 
+	//숫자 범위
+	while (1) {
+		printf("찾을 숫자를 입력하시오(단, 숫자는 1~50 사이의 숫자여야 합니다.): ");
+		scanf_s("%d", &target);
 
-//3번
-int add();
-
-void main()
-{
-	add();
-
-	printf("= %d", add());
-
-}
-
-int add() {
-	int n = 1;
-	int result;
-	int sum = 0;
-
-	for (; 2 * n < 100; n += 2) {
-		if (n == 1){
-			result = 2 * (n + (n + 1));
-			printf("%d ", result);
-			sum += result;
-		}
-		else {
-			result += 2 * (n + (n + 1));
-			printf("+ %d ", result);
-			sum += result;
+		if ((target < 1) || (target > 50)) {
+			printf("숫자를 잘못 입력하셨습니다.\n");
+			continue;
 		}
 
-	}
-
-	return sum;
-}
-
-
-
-//4번
-void draw_star();
-
-void main()
-{
-	draw_star();
-}
-
-void draw_star() {
-	int x, y;
-
-	for (int y = 0; y < 4; y++) {
-		for (int x = 0; x < 6; x++) {
-			if (((x >= 2) && (x <= 3) && (y >= 1) && (y <= 2)))
-				printf(" ");
-			else
-				printf("*");
-		}
-		printf("\n");
-	}
-}
-
-
-//6번
-void draw_pattern(int num);
-
-void main()
-{
-	int num;
-
-	printf("7 이상인 홀수를 입력하시오\n");
-	scanf_s("%d", &num);
-
-	draw_pattern(num);
-
-}
-
-void draw_pattern(int num) {
-	int i, j;
-	int cnt = 1;
-
-	for (i = 0; i < num; i++) {
-		if(i<num/2)
-			for (j = 0; j < num; j++) {
-				if (i + j >= num - 1)
-					break;
-				else
-					printf("%d ", cnt);
-				cnt++;
-			}
 		else
-			for(j=0; j<num; j++)
-		printf("\n");
+			break;
 	}
+
+	//target이 배열 안에 있는지 검사
+	for (int i = 0; i < 10; i++) {
+		if (target == arr[i])
+			count++;
+	}
+
+	printf("%d는 배열 안에 %d개 있습니다.\n", target, count);
 
 
 }
-
-
