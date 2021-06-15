00cf34262d247fc1031750df6918440fdc1c7ba8
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-06-15 21:54:30 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,65 +1,88 @@
-//배열 문제
+//반복문 문제
 #include <stdio.h>
 
-//문제 5. 시험 점수를 표로 출력하기
+//8번 문제
+//정수를 합산하는 프로그램
+/*for문 사용
 void main()
 {
-	int score[5][5] = { 0 };
+	int n, sum = 0;
 
-	//철수의 점수 입력받기
-	printf("철수의 점수 입력\n");
-	for (int i = 0; i < 4; i++) {
-		scanf_s("%d", &score[0][i]);
-	}
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &n);
 
-	//철희의 점수 입력받기
-	printf("철희의 점수 입력\n");
-	for (int i = 0; i < 4; i++) {
-		scanf_s("%d", &score[1][i]);
-	}
-
-	//영수의 점수 입력받기
-	printf("영수의 점수 입력\n");
-	for (int i = 0; i < 4; i++) {
-		scanf_s("%d", &score[2][i]);
-	}
-
-	//철희의 점수 입력받기
-	printf("철희의 점수 입력\n");
-	for (int i = 0; i < 4; i++) {
-		scanf_s("%d", &score[3][i]);
-	}
-
-
-	//과목별 총점 계산하기
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			score[4][i] += score[j][i];
+	if (n == 0)
+		printf("0\n");
+	else if (n < 0) {
+		for (int i = 2 * n; i <= n; i++) {
+			if (i == 2 * n) {
+				sum += i;
+				printf("(%d)", i);
+			}
+			else {
+				sum += i;
+				printf(" + (%d)", i);
+			}
 		}
+		printf(" = %d", sum);
 	}
-
-	//개인 총점 계산하기
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 4; j++) {
-			score[i][4] += score[i][j];
+	else {
+		for (int i = n; i <= 2 * n; i++) {
+			if (i == n) {
+				sum += i;
+				printf("%d", i);
+			}
+			else {
+				sum += i;
+				printf(" + %d", i);
+			}
 		}
+		printf(" = %d", sum);
 	}
-
-
-	//점수 출력하기
-	printf("철수 : %d, %d, %d, %d 개인 총점 : %d\n", score[0][0], score[0][1], score[0][2], score[0][3], score[0][4]);
-	printf("---------------------------------------\n");
-	printf("철희 : %d, %d, %d, %d 개인 총점 : %d\n", score[1][0], score[1][1], score[1][2], score[1][3], score[1][4]);
-	printf("---------------------------------------\n");
-	printf("영수 : %d, %d, %d, %d 개인 총점 : %d\n", score[2][0], score[2][1], score[2][2], score[2][3], score[2][4]);
-	printf("---------------------------------------\n");
-	printf("영희 : %d, %d, %d, %d 개인 총점 : %d\n", score[3][0], score[3][1], score[3][2], score[3][3], score[3][4]);
-	printf("---------------------------------------\n");
-	printf("과목 : %d, %d, %d, %d 총 합 : %d\n", score[4][0], score[4][1], score[4][2], score[4][3], score[4][4]);
-	printf("---------------------------------------\n");
-
 }
+*/
 
+/*while문 사용*/
+void main()
+{
+	int n, sum = 0;
+
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &n);
+
+	if (n == 0)
+		printf("0\n");
+	else if (n < 0) {
+		int i = 2 * n;
+		while (i <= n) {
+			if (i == 2 * n) {
+				sum += i;
+				printf("(%d)", i);
+			}
+			else {
+				sum += i;
+				printf(" + (%d)", i);
+			}
+			i++;
+		}
+		printf(" = %d", sum);
+	}
+	else {
+		int i = n;
+		while (i <= 2*n) {
+			if (i == n) {
+				sum += i;
+				printf("%d", i);
+			}
+			else {
+				sum += i;
+				printf(" + %d", i);
+			}
+		i++;
+		}
+		printf(" = %d", sum);
+	}
+}
 
 
 
