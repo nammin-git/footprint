ae1d8b24f202cefdc0ee7a2a4eb744a91bf7a441
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-06-18 21:17:56 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,74 +1,80 @@
-//책
-//10장 배열
 #include <stdio.h>
 
-//입력받은 정수를 역순으로 출력하기
-
+//그래프 만들기
+#define STUDENT 7
 void main()
 {
-	//입력받아서 배열에 저장
-	int arr[5] = { 0 };
+	//점수 입력받기
+	int grade[STUDENT] = { 0 };
 	
-	for (int i = 0; i < 5; i++) {
-		printf("정수를 입력하시오: ");
-		scanf_s("%d", &arr[i]);
+	for (int i = 0; i < STUDENT; i++) {
+		printf("학생 %d의 점수를 입력하시오: ", i + 1);
+		scanf_s("%d", &grade[i]);
 	}
 
-	//역순으로 출력
-	for (int i = 5; i > 0; i--) {
-		printf("%d\n", arr[i - 1]);
-	}
-}
-
-
-//막대그래프로 값 나타내기
-#define STUDENTS 5
-
-void main(void)
-{
-	int grade[STUDENTS] = { 30,20,10,40,50 };
-
-	for (int i = 0; i < STUDENTS; i++) {
-		printf("번호 %d: ", i);
+	//그래프로 출력하기
+	printf("\n그래프를 출력합니다.\n\n");
+	for (int i = 0; i < STUDENT; i++) {
+		printf("학생 %d: ", i + 1);
 		for (int s = 0; s < grade[i]; s++) {
 			printf("*");
 		}
 		printf("\n");
 	}
-
-
 }
 
 
-//주사위를 던져 나온 면 세기
-#include <stdlib.h>
-#include <time.h>
-#define SIZE 6
+//매개변수와 함수
+#define STUDENTS 5
+
+int get_average(int score[], int n);
+
+void main(void)
+{
+	int grade[STUDENTS] = { 1,2,3,4,5 };
+	int avg;
+
+	avg = get_average(grade, STUDENTS);
+	printf("평균은 %d입니다.\n", avg);
+}
+
+int get_average(int score[], int n) {
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < n; i++)
+		sum += score[i];
+
+	return sum / n;
+}
+
+
+#define SIZE 7
+
+void square_array(int a[], int size);
+void print_array(int a[], int size);
 
 void main()
 {
-	int dice[SIZE] = { 0 };
-	int d_face;
+	int list[SIZE] = { 1,2,3,4,5,6,7 };
 
-	srand((unsigned)time(NULL));
+	print_array(list, SIZE);
+	square_array(list, SIZE);
+	print_array(list, SIZE);
+}
 
-	for (int i = 0; i < 10000; i++) {
-		d_face = rand() % SIZE + 1;
-		dice[d_face - 1]++;
-	}
+void square_array(int a[], int size) {
+	for (int i = 0; i < size; i++)
+		a[i] = a[i] * a[i];
+}
 
-	printf("====================\n");
-	printf("면		빈도\n");
-	printf("====================\n");
-	for (int i = 0; i < SIZE; i++) {
-		printf("%d		%d\n", i + 1, dice[i]);
-	}
+void printf_array(int a[], int size) {
+	for (int i = 0; i < size; i++)
+		printf("%3d ", a[i]);
 }
 
 
-//tic-tac-toc 게임
 
-void main()
-{
 
-}
+
+
