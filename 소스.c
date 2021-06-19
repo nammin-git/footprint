d313e1aa0f540b998abc077a6ed7f86cd6b3c418
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-06-19 23:58:54 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,51 +1,38 @@
 #include <stdio.h>
 
-//그래프 만들기
-#define STUDENT 7
+//modify.c
+#define SIZE 7
+
+//배열 원소들을 제곱
+void square_array(int a[], int size);
+//배열 원소들을 화면에 출력
+void print_array(int a[], int size);
+
 void main()
 {
-	//점수 입력받기
-	int grade[STUDENT] = { 0 };
-	
-	for (int i = 0; i < STUDENT; i++) {
-		printf("학생 %d의 점수를 입력하시오: ", i + 1);
-		scanf_s("%d", &grade[i]);
-	}
+	int list[SIZE] = { 1,2,3,4,5,6,7 };
 
-	//그래프로 출력하기
-	printf("\n그래프를 출력합니다.\n\n");
-	for (int i = 0; i < STUDENT; i++) {
-		printf("학생 %d: ", i + 1);
-		for (int s = 0; s < grade[i]; s++) {
-			printf("*");
-		}
-		printf("\n");
-	}
+	print_array(list, SIZE);
+	square_array(list, SIZE);
+	print_array(list, SIZE);	//제곱한 값이 출력됨
+
 }
 
+//배열 원소 제곱하기
+void square_array(int a[], int size) {
+	for (int i = 0; i < size; i++) {
+		a[i] = a[i] * a[i];
+	}
 
-//매개변수와 함수
-#define STUDENTS 5
-
-int get_average(int score[], int n);
-
-void main(void)
-{
-	int grade[STUDENTS] = { 1,2,3,4,5 };
-	int avg;
-
-	avg = get_average(grade, STUDENTS);
-	printf("평균은 %d입니다.\n", avg);
 }
 
-int get_average(int score[], int n) {
-	int i;
-	int sum = 0;
+//배열 원소 그냥 출력
+void print_array(int a[], int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%3d ", a[i]);
+	}
+	printf("\n");
 
-	for (i = 0; i < n; i++)
-		sum += score[i];
-
-	return sum / n;
 }
 
 
@@ -60,21 +47,21 @@ void main()
 
 	print_array(list, SIZE);
 	square_array(list, SIZE);
-	print_array(list, SIZE);
+
 }
 
+//배열 원소 제곱한 값을 화면에 출력
 void square_array(int a[], int size) {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++) {
 		a[i] = a[i] * a[i];
-}
-
-void printf_array(int a[], int size) {
-	for (int i = 0; i < size; i++)
 		printf("%3d ", a[i]);
+	}
 }
 
-
-
-
-
-
+//배열 원소 그냥 출력
+void print_array(int a[], int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%3d ", a[i]);
+	}
+	printf("\n");
+}
