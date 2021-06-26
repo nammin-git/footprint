c7c7e65f9d46fef61c65b17c55c169bc56b4a6bd
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-06-26 21:33:46 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,102 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/*
-//배열 a를 배열 b로 복사하는 프로그램
-#define SIZE 5
 
-void array_copy(int a[], int b[], int size);
+//2차원 표를 배열로 작성하기
+#define ROWS 3
+#define COLS 5
 
 void main()
 {
-	int a[SIZE] = { 5,4,3,2,1 };
-	int b[SIZE] = { 0 };
-
-	printf("배열 a의 원소\n");
-	for (int i = 0; i < SIZE; i++) {
-		printf("%d ", a[i]);
-	}
-	printf("\n-------------\n");
-
-	printf("배열 b의 원소\n");
-	for (int i = 0; i < SIZE; i++) {
-		printf("%d ", b[i]);
-	}
-	printf("\n-------------\n");
-
-	printf("배열 a의 원소를 배열 b로 복사합니다.\n");
-	array_copy(a, b, SIZE);
-
-	printf("배열 b의 원소\n");
-	for (int i = 0; i < SIZE; i++) {
-		printf("%d ", b[i]);
-	}
-	printf("\n-------------\n");
-}
-
-void array_copy(int a[], int b[], int size) {
-	for (int i = 0; i < size; i++) {
-		b[i] = a[i];
-	}
-}
-*/
-/**/
-//가장 많이 생성된 난수를 출력하는 프로그램
-#define SIZE 100
-
-int print_array(int a[], int size);
-int get_max_count(int a[], int size);
-
-void main()
-{
-	int array[SIZE] = { 0 };
-
-	srand((unsigned)time(NULL));
-
-	for (int i = 0; i < SIZE; i++) {
-		array[i] = rand() % 10;
-	}
-
-	print_array(array, SIZE);
-
-	printf("\n-----------------------\n");
+	//배열 초기화
+	int array[ROWS][COLS] = {
+		{12,56,32,16,98},
+		{99,56,34,41,3},
+		{65,3,87,78,21}
+	};
 	
-	printf("가장 많이 나온 수는 %d입니다.\n", get_max_count(array, SIZE));
-}
-
-int print_array(int a[], int size) {
-	for (int i = 0; i < size; i++) {
-		if ((i % 10 == 0) && (i != 0)) {
-			printf("\n");
-			printf("%d  ", a[i]);
+	//배열 출력
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			printf("%3d ", array[i][j]);
 		}
-		else
-			printf("%d  ", a[i]);
-	}
-}
-
-int get_max_count(int a[], int size) {
-	int count[SIZE] = { 0 };
-
-	for (int i = 0; i < size; i++) {
-		count[a[i]]++;
+		printf("\n");
 	}
 
-	printf("숫자   나온 횟수\n");
-	for (int j = 0; j < 10; j++) {
-		printf("%3d	%6d\n", j, count[j]);
+	printf("=============\n");
+
+	//행의 합계
+	int sum = 0;
+
+	for (int i = 0; i < ROWS; i++) {
+		for (int col = 0; col < COLS; col++) {
+			sum += array[i][col];
+		}
+		printf("%d행의 합계= %d\n", i, sum);
+		sum = 0;
 	}
 
-	printf("-----------------------\n");
+	printf("=============\n");
 
-	int max_count=0;
-
-	for (int k = 0; k < 10; k++) {
-		if (count[k] > count[max_count])
-			max_count = k;
+	//열의 합계
+	for (int j = 0; j < COLS; j++) {
+		for (int row = 0; row < ROWS; row++) {
+			sum += array[row][j];
+		}
+		printf("%d열의 합계= %d\n", j, sum);
+		sum = 0;
 	}
 
-	printf("가장 많이 나온 횟수는 %d회 입니다.\n", count[max_count]);
-
-	return max_count;
 }
