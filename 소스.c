ef4dc9051f2e3a70eceb247a1389147db34e1fac
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-07-06 23:58:44 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,61 +1,25 @@
-//포인터
+//간접참조 *
 #include <stdio.h>
 
 void main()
 {
-	//주소연산자 &
-	int i = 10;
-	char c = 69;
-	double f = 12.3;
+	//int* p = 10000;
 
-	printf("i의 주소 = %u\n", &i);
-	printf("c의 주소 = %u\n", &c);
-	printf("f의 주소 = %u\n", &f);
+	//변수와 포인터를 연결한 후
+	//변수의 주소와 포인터의 값을 출력
+	int i = 3000;
+	int* p = NULL;
 
+	p = &i;
 
-	//어떤 자료형을 가리키는 포인터
-	char* pc;
-	float* pf;
-	double* pd;
-}
+	printf("변수 직접 참조\n");
+	printf("i = %d\n", i);
+	printf("&i = %u\n", &i);
 
-int main()
-{
-	int i = 0;
-	int* p = &i;
-
-	printf("i의 주소 = %u\n", p);
-
-	return 0;
-}
-
-
-void main()
-{
-	char c = 'A';
-	float f = 36.5;
-	double d = 3.141592;
-
-	char* pc = &c;
-	float* pf = &f;
-	double* pd = % d;
-}
-
-
-//포인터를 선언하고 변수의 주소를 포인터에 대입하기
-void main()
-{
-	int i = 10;
-	double f = 12.3;
-	int* pi = NULL;		//NULL은 주소 0
-	double* pf = NULL;
-
-	pi = &i;
-	pf = &f;
-
-	printf("%u %u\n", pi, &i);
-	printf("%u %u\n", pf, &f);
+	printf("--------------\n");
+	printf("포인터를 이용한 간접 참조\n");
+	printf("*p = %d\n", *p);
+	printf("p = %u\n", p);
 
 }
-
 
