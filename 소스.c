cfc6d77b458d5691c1ecc073faf1fbe29c19c8bf
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-07-08 23:24:12 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,25 +1,76 @@
-//간접참조 *
 #include <stdio.h>
+
+void main(void)
+{
+	//do while
+	int i = 1;
+	do {
+		printf("Hello World %d\n", i++);
+	} while (i <= 10);
+	
+
+	//2중 반복문
+	for (int i = 0; i <= 3; i++) {
+		printf("첫 번째 반복문 : %d\n", i);
+
+		for (int j = 0; j <= 5; j++) {
+			printf("	두 번째 반복문 : %d\n", j);
+		}
+	}
+}
+
+
+//이중 반복문 구구단
+void main()
+{	// 2 x 1 = 2
+	// 2 x 2 = 4
+	// 2 x 3 = 6
+	//...
+	// 9 x 9 = 81
+	for (int i = 2; i <= 9; i++) {
+		printf("%d단 계산\n", i);
+		for (int j = 1; j <= 9; j++) {
+			printf("   %d x %d = %d\n", i, j, i * j);
+		}
+	}
+}
+
+
+//별 출력하기
+// *
+// **
+// ***
+// ****
+// *****
 
 void main()
 {
-	//int* p = 10000;
-
-	//변수와 포인터를 연결한 후
-	//변수의 주소와 포인터의 값을 출력
-	int i = 3000;
-	int* p = NULL;
-
-	p = &i;
-
-	printf("변수 직접 참조\n");
-	printf("i = %d\n", i);
-	printf("&i = %u\n", &i);
-
-	printf("--------------\n");
-	printf("포인터를 이용한 간접 참조\n");
-	printf("*p = %d\n", *p);
-	printf("p = %u\n", p);
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i >= j)
+				printf("*");
+			else
+				continue;
+		}
+		printf("\n");
+	}
 
 }
 
+//     *
+//    **
+//   ***
+//  ****
+// *****
+void main()
+{
+	for (int i = 1; i <= 5; i++) {
+		for (int j = 5; j > 0; j--) {
+			if (i < j)
+				printf(" ");
+			else
+				printf("*");
+		}
+	printf("\n");
+	}
+}
