fd3f46c9ae24c74e75424b718d9d742b9384e04c
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-07-09 23:11:53 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,76 +1,77 @@
 #include <stdio.h>
 
-void main(void)
+//피라미드를 쌓아라
+//    *                         (1,5)7
+//   ***                   (2,4)(2,5)(2,6)9
+//  *****             (3,3)(3,4)(3,5)(3,6)(3,7)11
+// *******       (4,2)(4,3)(4,4)(4,5)(4,6)(4,7)(4,8)13
+//********* (5,1)(5,2)(5,3)(5,4)(5,5)(5,6)(5,7)(5,8)(5,9)
+
+void main()
 {
-	//do while
-	int i = 1;
-	do {
-		printf("Hello World %d\n", i++);
-	} while (i <= 10);
+	int floor;
 	
+	printf("몇 층으로 쌓겠느냐? ");
+	scanf_s("%d", &floor);
 
-	//2중 반복문
-	for (int i = 0; i <= 3; i++) {
-		printf("첫 번째 반복문 : %d\n", i);
+	int i, j;
 
-		for (int j = 0; j <= 5; j++) {
-			printf("	두 번째 반복문 : %d\n", j);
-		}
-	}
-}
-
-
-//이중 반복문 구구단
-void main()
-{	// 2 x 1 = 2
-	// 2 x 2 = 4
-	// 2 x 3 = 6
-	//...
-	// 9 x 9 = 81
-	for (int i = 2; i <= 9; i++) {
-		printf("%d단 계산\n", i);
-		for (int j = 1; j <= 9; j++) {
-			printf("   %d x %d = %d\n", i, j, i * j);
-		}
-	}
-}
-
-
-//별 출력하기
-// *
-// **
-// ***
-// ****
-// *****
-
-void main()
-{
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			if (i >= j)
+	for (i = 1; i <= floor; i++) {
+		for (j = 1; j < 2 * floor; j++) {
+			if ((j < floor) && (i + j <= floor))
+				printf(" ");
+			else if ((j < floor) && (i + j > floor))
 				printf("*");
-			else
-				continue;
+			else if (j == floor)
+				printf("*");
+			else if ((j > floor) && (j - i < floor))
+				printf("*");
+			else if ((j > floor) && (j - i >= floor))
+				printf(" ");
 		}
 		printf("\n");
 	}
-
 }
 
-//     *
-//    **
-//   ***
-//  ****
-// *****
-void main()
+
+void main(void)
 {
-	for (int i = 1; i <= 5; i++) {
-		for (int j = 5; j > 0; j--) {
-			if (i < j)
-				printf(" ");
-			else
-				printf("*");
-		}
-	printf("\n");
+	int floor;
+
+	printf("몇 층으로 쌓겠느냐? ");
+	scanf_s("%d", &floor);
+
+	for (int i = 0; i < floor; i++) {
+		for (int j = i; j < floor - 1; j++)
+			printf(" ");
+		for (int k = 0; k < 2 * i + 1; k++)
+			printf("*");
+		printf("\n");
 	}
 }
+
+
+//버스를 탄다고 가정
+//학생과 일반인으로 구분 (일반인 : 20세 이상)
+void main()
+{
+	int age = 15;
+	if (age >= 20)
+		printf("일반인입니다.\n");
+	else
+		printf("학생입니다.\n");
+
+//학생을 초등학생(8~13) / 중학생(14~16) / 고등학생(17~19)로 나누면?
+	if (age >= 8 && age <= 13) {
+		printf("초등학생입니다.\n");
+	}
+	else if (age >= 14 && age <= 16) {
+		printf("중학생입니다.\n");
+	}
+	else if (age >= 17 && age <= 13) {
+		printf("고등학생입니다.\n");
+	}
+	else
+		printf("학생이 아닙니다.\n");
+}
+
