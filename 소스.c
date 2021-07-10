d2b1d95aab75b78aa0b92224b4cc9ba7c33a80ac
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-07-10 23:58:09 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,77 +1,132 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-//피라미드를 쌓아라
-//    *                         (1,5)7
-//   ***                   (2,4)(2,5)(2,6)9
-//  *****             (3,3)(3,4)(3,5)(3,6)(3,7)11
-// *******       (4,2)(4,3)(4,4)(4,5)(4,6)(4,7)(4,8)13
-//********* (5,1)(5,2)(5,3)(5,4)(5,5)(5,6)(5,7)(5,8)(5,9)
-
+//break와 continue
 void main()
 {
-	int floor;
-	
-	printf("몇 층으로 쌓겠느냐? ");
-	scanf_s("%d", &floor);
-
-	int i, j;
-
-	for (i = 1; i <= floor; i++) {
-		for (j = 1; j < 2 * floor; j++) {
-			if ((j < floor) && (i + j <= floor))
-				printf(" ");
-			else if ((j < floor) && (i + j > floor))
-				printf("*");
-			else if (j == floor)
-				printf("*");
-			else if ((j > floor) && (j - i < floor))
-				printf("*");
-			else if ((j > floor) && (j - i >= floor))
-				printf(" ");
+	//1번부터 30번까지 있는 반
+	//1번에서 5번까지 조별 발표
+	for (int i = 1; i <= 30; i++) {
+		if (i >= 6) {
+			printf("나머지 학생은 집에 가세요.\n");
+			break;
 		}
-		printf("\n");
+		printf("%d 번 학생은 조별 발표 준비를 하세요.\n", i);
+	}
+
+	//7번 학생이 아파서 결석
+	//7번 학생을 제외하고 6번부터 10번까지 조별 발표
+	for (int i = 1; i <= 30; i++) {
+		if (i >= 6&&i<=10) {
+			if (i == 7) {
+				printf("%d 번 학생은 결석입니다.\n", i);
+				continue;
+			}
+			printf("%d 번 학생은 조별 발표 준비를 하세요.\n", i);
+		}		
 	}
 }
 
 
-void main(void)
+//랜덤
+int main(void)
 {
-	int floor;
+	printf("난수 초기화 이전\n");
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", rand() % 10);
+	}
 
-	printf("몇 층으로 쌓겠느냐? ");
-	scanf_s("%d", &floor);
+	srand((unsigned)time(NULL));
+	printf("\n난수 초기화 이후\n");
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", rand() % 10);
+	}
+	return 0;
+}
 
-	for (int i = 0; i < floor; i++) {
-		for (int j = i; j < floor - 1; j++)
-			printf(" ");
-		for (int k = 0; k < 2 * i + 1; k++)
-			printf("*");
-		printf("\n");
+//가위 바위 보(0 1 2)
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int i = rand() % 3;
+
+	if (i == 0) {
+		printf("가위\n");
+	}
+	else if (i == 1) {
+		printf("바위\n");
+	}
+	else if (i == 2) {
+		printf("보\n");
+	}
+	else {
+		printf("몰라요\n");
+	}
+
+	switch (i) {
+	case 0:printf("가위\n"); break;
+	case 1:printf("바위\n"); break;
+	case 2:printf("보\n"); break;
+	default: printf("몰라요\n"); break;
 	}
 }
 
 
-//버스를 탄다고 가정
-//학생과 일반인으로 구분 (일반인 : 20세 이상)
-void main()
+//교통카드
+int main()
 {
 	int age = 15;
-	if (age >= 20)
-		printf("일반인입니다.\n");
-	else
-		printf("학생입니다.\n");
+	switch (age) {
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	case 12:
+	case 13:printf("초등학생입니다\n"); break;
+	case 14:
+	case 15:
+	case 16:printf("중학생입니다\n"); break;
+	case 17:
+	case 18:
+	case 19:printf("고등학생입니다\n"); break;
+	default: printf("학생이 아닌가봐요\n"); break;
+	}
 
-//학생을 초등학생(8~13) / 중학생(14~16) / 고등학생(17~19)로 나누면?
-	if (age >= 8 && age <= 13) {
-		printf("초등학생입니다.\n");
-	}
-	else if (age >= 14 && age <= 16) {
-		printf("중학생입니다.\n");
-	}
-	else if (age >= 17 && age <= 13) {
-		printf("고등학생입니다.\n");
-	}
-	else
-		printf("학생이 아닙니다.\n");
+	return 0;
 }
 
+
+//Up and Down
+int main()
+{
+	srand((unsigned)time(NULL));
+	
+	int answer = 1 + rand() % 100;
+	int n;
+
+	for (int chance = 0; chance < 5; chance++) {
+		printf("숫자를 입력하시오: ");
+		scanf_s("%d", &n);
+		
+		if (answer < n) {
+			printf("Down ↓\n");
+		}
+		else if (answer == n) {
+			printf("You Win!\n");
+			break;
+		}
+		else if (answer > n) {
+			printf("Up ↑\n");
+		}
+		else
+			printf("알 수 없는 오류가 발생했어요\n\n");
+
+		if (chance == 4) {
+			printf("You Lose\n");
+		}
+
+	}
+	
+}
