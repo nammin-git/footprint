0b888502299a1c54f04db60e5c141848fea77acc
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-07-17 18:51:24 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,120 +1,85 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 #define BOTTLE 4
+
+int GetTreatment();
+int GetRandomBottle(int count, int treatment);
 
 void main()
 {
+	printf("\n\n === 아빠는 대머리 게임 ===\n\n");
+	Sleep(1000);
+	printf("대머리인 아빠를 위해 발모제를 찾아주세요 !!\n\n\n");
+	Sleep(1000);
+
+	//발모제 지정하기
 	srand((unsigned)time(NULL));
+	int treatment=GetTreatment(BOTTLE);
 
-	printf("\n\n === 아빠는 대머리 게임 === \n\n");
+	//병들을 머리에 뿌리며 머리가 나는지 아닌지를 봅니다(3번 실행)
+	//보여줄 병들을 선택
+	printf("일단 머리에 병들을 뿌려볼게요.\n\n");
 
-	int answer;
-	int treatment = rand() % BOTTLE;
-	int isIncluded = 0;
-	int hint = 0;
-	char blank = 0;
-
-	//발모제 찾기 시도 
 	for (int i = 1; i <= 3; i++) {
-		//1차 시도 (숫자 하나 입력 가능)
-		printf("발모제를 찾아봅시다\n숫자 %d 개를 입력하세요(숫자들 사이는 띄어쓰기): ", i);
-
-		int j = 0;
-		do {
-			scanf_s("%d", &hint);
-			if (hint == treatment) {
-				isIncluded++;
-			}
-			j++;
-		} while (j <= i);
-		scanf_s("%c", &blank, 1);
+		printf("%d 회 시도\n\n", i);
+		if (GetRandomBottle(i, treatment) == 0) {
+			printf("머리가 났어요!\n\n");
+		}
+		else {
+			printf("머리가 나지 않았어요.\n\n");
+		}
+		printf("... 계속하려면 엔터 키를 눌러주세요 ...\n\n");
+		getchar();
 	}
 
+	//발모제 찾기
+	int answer;
 
-	//발모제 선택
-	printf("발모제를 선택하세요: ");
-	scanf_s("%d", answer);
+	printf("==================\n이제 발모제를 찾아봅시다.\n");
+	printf("발모제는 몇 번 병입니까?\n");
+	scanf_s("%d", &answer);
 
 	if (answer == treatment + 1) {
-		printf(" >> 성공 ! 발모제를 찾았습니다.\n 축하해요 !! 머리가 자랐네요 !\n");
+		printf("\n\n >> 성공 !\n축하합니다 ! 발모제를 찾았습니다 !!\n");
 	}
 	else {
-		printf(" >> 땡 ! 발모제는 %d번입니다...\n머리가 자라지 않았습니다ㅠㅠ\n");
+		printf("\n\n >> 땡 !\n발모제는 %d 번 병이었습니다 !\n아빠는 계속 대머리로 지내야 합니다 ...", treatment + 1);
 	}
 
 
 }
 
+int GetTreatment() {
+	//발모제를 랜덤으로 설정합니다
+	return rand() % BOTTLE;
+}
 
-int main(void) {
-	srand((unsigned)time(NULL));
 
-	printf("\n\n === 아빠는 대머리 게임 === \n\n");
+int GetRandomBottle(int count, int treatment) {
+	//보여줄 발모제를 선택합니다
+	int selectedBottle, prevSelectedBottle = 0;
+	int isIncluded = 0;
 
-	int answer;		//사용자 입력값
-	int treatment = rand() % 4;		//발모제 선택 (0~3)
-
-	int cntShowBottle = 0;	//이번 게임에 보여줄 병 갯수
-	int prevCntShowBottle = 0;	//앞 게임에 보여준 병 갯수
-	//보여주는 병 갯수를 차차 늘려줌으로써 정답률 향상
-
-	//3번의 기회 (3번의 발모제 투여 시도)
-	for (int i = 1; i <= 3; i++) {
-		int bottle[4] = { 0 };	//4개의 병
-		do {
-			cntShowBottle = rand() % 2 + 2;		//2~3개의 병이 뽑히도록 설정
-		} while (cntShowBottle == prevCntShowBottle);
-		prevCntShowBottle = cntShowBottle;
-
-		int isIncluded = 0;		//보여줄 병 중에 발모제가 포함되었는지 여부
-		printf(" > %d 번째 시도 : ", i);
-
-		//보여줄 병 종류를 선택
-		for (int j = 0; j < cntShowBottle; j++) {
-			int randBottle = rand() % 4;	//병을 뽑습니다
-
-			//아직 선택되지 않은 병이면 >>>> 선택 처리
-			if (bottle[randBottle] == 0) {
-				bottle[randBottle] = 1;
-				if (randBottle == treatment) {
-					isIncluded = 1;
-				}
-			}
-			//이미 선택된 병이면 >>>> 중복이므로 다시 선택
-			else {
-				j--;
-			}
+	printf("선택된 병은 ");
+	for (int j = 0; j < count; j++) {
+		selectedBottle = 1 + rand() % BOTTLE;
+		if (prevSelectedBottle == selectedBottle) {
+			j--;
+			continue;
 		}
 
-		//사용자에게 문제 표시
-		for (int k = 0; k < 4; k++) {
-			if (bottle[k] == 1)
-				printf("%d ", k + 1);
-		}
-		printf("물약을 머리에 바릅니다\n");
+		printf("%d ", selectedBottle);
 
-		if (isIncluded == 1) {
-			printf("  >> 성공! 머리가 났어요 !!\n");
-		}
-		else {
-			printf("  >> 실패! 머리가 나지 않았어요.. ㅠㅠ\n");
+		if (selectedBottle == treatment + 1) {
+			isIncluded++;
 		}
 
-		printf("\n ... 계속하려면 아무 키나 누르세요 ...");
-		getchar();
+		prevSelectedBottle = selectedBottle;
 	}
+	printf("입니다.\n머리에 뿌렸더니 ");
 
-	printf("\n\n발모제는 몇 번일까요? ");
-	scanf_s("%d", &answer);
-
-	if (answer == treatment + 1) {
-		printf("\n >> 정답입니다!\n");
-	}
-	else
-	{
-		printf("\n >> 땡 ! 틀렸어요, 정답은 %d 입니다!\n", treatment + 1);
-	}
-
+	return isIncluded;
 }
 
