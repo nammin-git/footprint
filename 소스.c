51089b17f87f60b9424f36aea4b862a02010a08a
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-07-19 21:02:34 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,85 +1,117 @@
+//포인터
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <windows.h>
-#define BOTTLE 4
-
-int GetTreatment();
-int GetRandomBottle(int count, int treatment);
 
 void main()
 {
-	printf("\n\n === 아빠는 대머리 게임 ===\n\n");
-	Sleep(1000);
-	printf("대머리인 아빠를 위해 발모제를 찾아주세요 !!\n\n\n");
-	Sleep(1000);
+	//같은 아파트에 사는 세 친구
+	//각 문 앞에 암호가 걸려 있음
+	//[철수] : 101호 --> 메모리 공간의 주소
+	//[영희] : 201호
+	//[민수] : 301호
 
-	//발모제 지정하기
-	srand((unsigned)time(NULL));
-	int treatment=GetTreatment(BOTTLE);
+	int 철수 = 1;
+	int 영희 = 2;
+	int 민수 = 3;
 
-	//병들을 머리에 뿌리며 머리가 나는지 아닌지를 봅니다(3번 실행)
-	//보여줄 병들을 선택
-	printf("일단 머리에 병들을 뿌려볼게요.\n\n");
-
-	for (int i = 1; i <= 3; i++) {
-		printf("%d 회 시도\n\n", i);
-		if (GetRandomBottle(i, treatment) == 0) {
-			printf("머리가 났어요!\n\n");
-		}
-		else {
-			printf("머리가 나지 않았어요.\n\n");
-		}
-		printf("... 계속하려면 엔터 키를 눌러주세요 ...\n\n");
-		getchar();
-	}
-
-	//발모제 찾기
-	int answer;
-
-	printf("==================\n이제 발모제를 찾아봅시다.\n");
-	printf("발모제는 몇 번 병입니까?\n");
-	scanf_s("%d", &answer);
-
-	if (answer == treatment + 1) {
-		printf("\n\n >> 성공 !\n축하합니다 ! 발모제를 찾았습니다 !!\n");
-	}
-	else {
-		printf("\n\n >> 땡 !\n발모제는 %d 번 병이었습니다 !\n아빠는 계속 대머리로 지내야 합니다 ...", treatment + 1);
-	}
+	printf("철수네 주소 : %d, 암호 : %d\n", &철수, 철수);
+	printf("영희네 주소 : %d, 암호 : %d\n", &영희, 영희);
+	printf("민수네 주소 : %d, 암호 : %d\n", &민수, 민수);
 
 
+	//미션맨 !
+
+	//첫 번째 미션 : 아파트의 각 집에 방문하여 문에 적힌 암호 확인
+	printf("\n ... 미션맨이 미션 수행하는 중 ...\n\n");
+
+	int* 미션맨;	//포인터 변수
+	미션맨 = &철수;
+	printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", 미션맨, *미션맨);
+
+	미션맨 = &영희;
+	printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", 미션맨, *미션맨);
+
+	미션맨 = &민수;
+	printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", 미션맨, *미션맨);
+
+
+	//첫 번째 미션 : 각 암호에 3을 곱해라
+	printf("\n ... 미션맨이 비밀번호를 바꾸는 중 ...\n\n");
+
+	미션맨 = &철수;
+	*미션맨 = *미션맨 * 3;
+	printf("미션맨이 암호를 바꾼 곳 주소 : %d, 암호 : %d\n", 미션맨, *미션맨);
+
+	미션맨 = &영희;
+	*미션맨 = *미션맨 * 3;
+	printf("미션맨이 암호를 바꾼 곳 주소 : %d, 암호 : %d\n", 미션맨, *미션맨);
+	
+	미션맨 = &민수;
+	*미션맨 = *미션맨 * 3;
+	printf("미션맨이 암호를 바꾼 곳 주소 : %d, 암호 : %d\n", 미션맨, *미션맨);
+
+
+	//스파이의 등장 !
+	//미션맨이 바꾼 암호에서 2를 빼라 !
+	int* 스파이 = 미션맨;
+	printf("\n ... 스파이가 미션 수행하는 중 ...\n\n");
+
+	스파이 = &철수;
+	*스파이 = *스파이 - 2;
+	printf("스파이가 방문하는 곳 주소 : %d, 암호 : %d\n", 스파이, *스파이);
+
+	스파이 = &영희;
+	*스파이 = *스파이 - 2;
+	printf("스파이가 방문하는 곳 주소 : %d, 암호 : %d\n", 스파이, *스파이);
+
+	스파이 = &민수;
+	*스파이 = *스파이 - 2;
+	printf("스파이가 방문하는 곳 주소 : %d, 암호 : %d\n", 스파이, *스파이);
+
+
+	printf("\n ... 철수 영희 민수는 집에 오고서는 바뀐 암호를 보고 깜놀 ...\n\n");
+
+	printf("철수네 주소 : %d, 암호 : %d\n", &철수, 철수);
+	printf("영희네 주소 : %d, 암호 : %d\n", &영희, 영희);
+	printf("민수네 주소 : %d, 암호 : %d\n", &민수, 민수);
+
+
+	//미션맨과 스파이가 사는 곳의 주소는?
+	printf("미션맨의 주소 : %d\n", &미션맨);
+	printf("스파이의 주소 : %d\n", &스파이);
 }
 
-int GetTreatment() {
-	//발모제를 랜덤으로 설정합니다
-	return rand() % BOTTLE;
-}
 
+//배열과 주소
+int main(void) {
+	int arr[3] = { 5,10,15 };
+	int* ptr = arr;
 
-int GetRandomBottle(int count, int treatment) {
-	//보여줄 발모제를 선택합니다
-	int selectedBottle, prevSelectedBottle = 0;
-	int isIncluded = 0;
-
-	printf("선택된 병은 ");
-	for (int j = 0; j < count; j++) {
-		selectedBottle = 1 + rand() % BOTTLE;
-		if (prevSelectedBottle == selectedBottle) {
-			j--;
-			continue;
-		}
-
-		printf("%d ", selectedBottle);
-
-		if (selectedBottle == treatment + 1) {
-			isIncluded++;
-		}
-
-		prevSelectedBottle = selectedBottle;
+	for (int i = 0; i < 3; i++) {
+		printf("배열 arr[%d]의 값 : %d\n", i, arr[i]);
 	}
-	printf("입니다.\n머리에 뿌렸더니 ");
 
-	return isIncluded;
+	for (int j = 0; j < 3; j++) {
+		printf("포인터 ptr[%d]의 값 : %d\n", j, ptr[j]);
+	}
+
+	printf("\n ... ptr의 값을 바꾸는 중 ...\n\n");
+
+	ptr[0] = 100;
+	ptr[1] = 200;
+	ptr[2] = 300;
+
+	for (int i = 0; i < 3; i++) {
+		printf("배열 arr[%d]의 값 : %d\n", i, arr[i]);
+		printf("배열 arr[%d]의 값 : %d\n\n", i, *(arr+i));
+	}
+
+	for (int j = 0; j < 3; j++) {
+		printf("포인터 ptr[%d]의 값 : %d\n", j, ptr[j]);
+		printf("포인터 ptr[%d]의 값 : %d\n\n", j, *(ptr+j));
+	}
+	
+	//*(arr+i) == arr[i]
+	//arr==arr배열의 첫번째 값의 주소와 동일 == &arr[0]
+
 }
 
