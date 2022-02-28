0842fc84b9bcc8db69be785c8423419ce283a260
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-02-28 23:47:32 +0900

    Add files via upload

---
+++ ListView/app/src/main/java/com/mgprogect/maximlist/ListViewAdapter.kt
@@ -0,0 +1,50 @@
+package com.mgprogect.listview_ex
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class ListViewAdapter(val List : MutableList<String> /*문자열 데이터를 받겠습니다)*/) : BaseAdapter()/*베이스 어댑터의 자식입니다*/ {
+    /*
+    * 원하는 것: 리스트에 각각의 데이터를 보내고 싶다
+    * 메인 액티비티에서 어댑터로 [a, b, c] 라는 데이터를 보냅니다
+    * 어댑터는 리스트에 하나하나씩 값을 넣어주는 역할을 한다
+    * 그래서 [a, b, c]는 리스트에 차례차례 전달된다
+    * 리스트1에 a, 리스트2에 b, 리스트3에 c가 들어감
+    */
+    override fun getCount(): Int {
+        //리스트뷰의 갯수 = 리스트가 몇 줄 뜰건지
+        return List.size    //리스트 크기 만큼만 줄 뜨게 하겠다
+    }
+
+    override fun getItem(position: Int): Any {
+        //리스트의 아이템을 가져다 줌
+        //딱히 신경 안 써도 됨
+        return List[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        //딱히 신경 안 써도 됨
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        //데이터를 받아와서 몇 번째줄 리스트에 넣을지 정함
+        //리스트뷰 파일을 만들어줘야지 리스트에 넣겠죠? -> xml파일 만듦
+
+        //convertView: View? 를 이용해서 데이터와 리스트를 연결해줌
+        var convertView_join = convertView
+
+        //listView_file 에 있는 형식를 가져오는 부분
+        if(convertView_join == null) {
+            convertView_join = LayoutInflater.from(parent?.context).inflate(R.layout.listview_file, parent, false)
+        }
+
+        //라인을 하나 가져온 다음에 컨버트뷰를 되돌려줌
+        return convertView_join!!
+
+    }
+
+}
---
+++ ListView/app/src/main/java/com/mgprogect/maximlist/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.mgprogect.listview_ex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mgprogect.listview_ex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        //데이터 만들기
+        val listFruit = mutableListOf<String>()
+
+        listFruit.add("apple")
+        listFruit.add("banana")
+        listFruit.add("banana")
+        listFruit.add("pear")
+
+        //메인엑티비티 >> 어댐터 ... 데이터 넘겨주기
+        val adapter = ListViewAdapter(listFruit)
+
+        //메인 엑티비티 xml 파일에 있는 리스트뷰를 변수에 넣기
+        val listview = binding.mainListView
+        listview.adapter = adapter  //activity_main.xml에 있는 어댑터에 MainActivity와 ListViewAdapter에 있는 어댑터를 서로 같은 거라고 인식시켜줌
+
+
+    }
+}
