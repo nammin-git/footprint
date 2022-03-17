2afa814c327f0f68b29323882a05c00407b28623
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-03-17 23:58:13 +0900

    Add files via upload

--- MovieList/app/src/main/java/com/mgprogect/movielist/MovieClass.kt
+++ MovieList/app/src/main/java/com/mgprogect/movielist/MovieClass.kt
@@ -1,12 +1,8 @@
 package com.mgprogect.movielist
 
+import android.graphics.drawable.Drawable
+
 data class MovieClass(
-    var poster: Int,
+    var poster: Drawable,
     val movieName: String,
     val grade: String)
-
-val movie1 = MovieClass(R.drawable.the_truman_poster, "The Truman Show", "PG 12")
-val movie2 = MovieClass(R.drawable.the_silence_poster, "The Silence Of The Lambs", "X-rated")
-val movie3 = MovieClass(R.drawable.lala_land_poster, "LaLaLand", "PG 12")
-
-
