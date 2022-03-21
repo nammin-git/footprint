1544070406dfa85c4f4f53c0138f161e18cb06ac
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-03-21 23:59:25 +0900

    Add files via upload

---
+++ Listview_Sns/main/ListViewItem.kt
@@ -0,0 +1,8 @@
+package com.mgprogect.listview_sns
+
+import android.graphics.drawable.Drawable
+
+data class ListViewItem(
+    val icon: Drawable,
+    val title: String,
+    val subTitle: String)
---
+++ Listview_Sns/main/ListviewAdapter.kt
@@ -0,0 +1,23 @@
+package com.mgprogect.listview_sns
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ListViewAdapter(val items: MutableList<ListViewItem>): BaseAdapter() {
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+}
