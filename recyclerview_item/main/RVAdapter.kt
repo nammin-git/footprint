ccfb7d3c84ec385206656d816980e7f6df2ac0b8
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-03-27 00:09:44 +0900

    Add files via upload

---
+++ recyclerview_item/main/RVAdapter.kt
@@ -0,0 +1,19 @@
+package com.mgprogect.recyclerview_item
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RVAdapter(val items: MutableList<String>): RecyclerView.Adapter<RVAdapter> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVAdapter {
+        //리사이클러뷰
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RVAdapter, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
