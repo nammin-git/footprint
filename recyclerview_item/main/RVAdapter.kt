f36c5cdeb185727b5bebf95f92928db2f2eca991
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-03-30 23:40:14 +0900

    Add files via upload

--- recyclerview_item/main/RVAdapter.kt
+++ recyclerview_item/main/RVAdapter.kt
@@ -1,19 +1,28 @@
 package com.mgprogect.recyclerview_item
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class RVAdapter(val items: MutableList<String>): RecyclerView.Adapter<RVAdapter> {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVAdapter {
-        //리사이클러뷰
+class RVAdapter(val items: MutableList<String>): RecyclerView.Adapter<RVAdapter.ViewHolder> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVAdapter.ViewHolder {
+        //리사이클러뷰에 아이템을 가져와줌
         TODO("Not yet implemented")
     }
 
-    override fun onBindViewHolder(holder: RVAdapter, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: RVAdapter.ViewHolder, position: Int) {
+        //뷰홀더에 바인딩해줌
+        holder.bindItems(items[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        //전체 리사이클러뷰의 갯수를 알려줌
+        return items.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bindItems(item: String) {
+
+        }
     }
 }
