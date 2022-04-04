40183e8d0967760bcc29e6ac39db13698391d2c7
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-04-04 23:58:59 +0900

    Add files via upload

---
+++ nav_fragment/main/SecondFragment.kt
@@ -0,0 +1,35 @@
+package com.mgprogect.nav_fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class SecondFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+
+        view.findViewById<Button>(R.id.btn1).setOnClickListener {
+
+        }
+
+        view.findViewById<Button>(R.id.btn2).setOnClickListener {
+
+        }
+
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+}
