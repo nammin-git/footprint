b0d08fb32202fd7b859a383b89e95a3680b0844c
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-04-08 01:43:14 +0900

    Add files via upload

--- trot_list/main/MainActivity.kt
+++ trot_list/main/MainActivity.kt
@@ -2,16 +2,19 @@ package com.mgprogect.trot_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mgprogect.trot_list.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         //1. navigation 만들기
 
         //2. fragment 만들기
-        //2-1. fragment 를 navigation 에 연결
+            //2-1. fragment 를 navigation 에 연결
+            //2-2. fragment 에서 버튼 누르면 이동하기
 
         //3. recyclerView 만들기
     }
