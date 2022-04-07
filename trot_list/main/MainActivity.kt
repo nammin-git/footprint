f0ea62d4c3dfcafdf7634499939b3c220efdf268
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-04-08 00:00:14 +0900

    Add files via upload

---
+++ trot_list/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mgprogect.trot_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //1. navigation 만들기
+
+        //2. fragment 만들기
+        //2-1. fragment 를 navigation 에 연결
+
+        //3. recyclerView 만들기
+    }
+}
