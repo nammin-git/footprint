4e0b9748378014f440e0cfbecc2536e20854a631
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-04-19 23:58:46 +0900

    Add files via upload

--- fb_login_test/app/src/main/java/com/mgprogect/fb_login_test/MainActivity.kt
+++ fb_login_test/app/src/main/java/com/mgprogect/fb_login_test/MainActivity.kt
@@ -2,10 +2,18 @@ package com.mgprogect.fb_login_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mgprogect.fb_login_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val btn = binding.noEmailLoginBtn
+        btn.setOnClickListener {
+
+        }
+
     }
 }
