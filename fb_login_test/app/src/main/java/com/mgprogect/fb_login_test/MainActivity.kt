a3cccf82e66b3d2d3b200d271a6de131b865271d
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-04-20 00:15:16 +0900

    Add files via upload

--- fb_login_test/app/src/main/java/com/mgprogect/fb_login_test/MainActivity.kt
+++ fb_login_test/app/src/main/java/com/mgprogect/fb_login_test/MainActivity.kt
@@ -2,16 +2,36 @@ package com.mgprogect.fb_login_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.mgprogect.fb_login_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
         val btn = binding.noEmailLoginBtn
         btn.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        Log.d("MainActivity", user!!.uid)
+                    } else {
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
 
         }
 
