747fa9e72c29a3e79881f6b36359bc6bc5e9ee0d
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-04-23 00:01:10 +0900

    Add files via upload

--- fb_email_password_auth/app/src/main/java/com/mgprogect/fb_email_password_auth/MainActivity.kt
+++ fb_email_password_auth/app/src/main/java/com/mgprogect/fb_email_password_auth/MainActivity.kt
@@ -5,11 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
 
         val joinBtn = findViewById<Button>(R.id.joinBtn)
         joinBtn.setOnClickListener {
@@ -19,6 +26,19 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("main", email.text.toString())
             Log.d("main", password.text.toString())
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(this, "성공", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
 
         }
     }
