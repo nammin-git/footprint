22f9dcc6ca2ac943c6c876b4cfaf29e737d7a244
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-04-30 00:11:48 +0900

    Add files via upload

--- diet_memo/app/src/main/java/com/mgprogect/diet_memo/MainActivity.kt
+++ diet_memo/app/src/main/java/com/mgprogect/diet_memo/MainActivity.kt
@@ -2,10 +2,30 @@ package com.mgprogect.diet_memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val writeBtn = findViewById<ImageView>(R.id.writeBtn)
+        writeBtn.setOnClickListener {
+            //dialog를 띄우는 코드(22~27)
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("운동 메모 다이얼로그")
+
+            mBuilder.show()
+        }
+
     }
 }
