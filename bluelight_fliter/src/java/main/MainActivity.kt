d2615a4abf66da542e216c5dddf6dfa2aae21297
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-06-21 23:44:51 +0900

    Add files via upload

--- bluelight_fliter/src/java/main/MainActivity.kt
+++ bluelight_fliter/src/java/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mgprogect.bluelight_filter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +10,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val slider = findViewById<Slider>(R.id.slider)
+        slider.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: Slider) {
+                // Responds to when slider's touch event is being started
+            }
+
+            override fun onStopTrackingTouch(slider: Slider) {
+                // Responds to when slider's touch event is being stopped
+            }
+        })
+
+        slider.addOnChangeListener { slider, value, fromUser ->
+            // Responds to when slider's value is changed
+        }
 
 
     }
