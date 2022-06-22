eb2883684ecfa988a093b6430408d2b0251b70ae
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-06-22 23:58:28 +0900

    Add files via upload

--- bluelight_fliter/src/java/main/MainActivity.kt
+++ bluelight_fliter/src/java/main/MainActivity.kt
@@ -2,19 +2,23 @@ package com.mgprogect.bluelight_filter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.System.SCREEN_BRIGHTNESS
 import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        private var brightness = null
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val slider = findViewById<Slider>(R.id.slider)
         slider.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
             override fun onStartTrackingTouch(slider: Slider) {
-                // Responds to when slider's touch event is being started
+                brightness = System.getInt(contentResolver, System.SCREEN_BRIGHTNESS)
             }
 
             override fun onStopTrackingTouch(slider: Slider) {
