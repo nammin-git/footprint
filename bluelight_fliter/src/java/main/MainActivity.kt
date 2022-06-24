fd9e7d7af7eaa33afcb5628293301b05339c898b
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-06-24 23:53:44 +0900

    Add files via upload

--- bluelight_fliter/src/java/main/MainActivity.kt
+++ bluelight_fliter/src/java/main/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.mgprogect.bluelight_filter
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.provider.Settings.System.SCREEN_BRIGHTNESS
+import android.provider.Settings.System.canWrite
 import android.util.Log
 import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatDrawableManager.get
 import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
@@ -26,15 +33,28 @@ class MainActivity : AppCompatActivity() {
         val seekbar = findViewById<SeekBar>(R.id.seekBar)
         val textPerc = findViewById<TextView>(R.id.textPercentage)
 
+        //If app has no permission to write system settings
+        if(!canWrite) {
+            seekbar.isEnabled = false
+            allowWritePermission()
+        }
+
         //seekbar setting
-        seekbar.max = 255
+        seekbar.max = 100
         seekbar.keyProgressIncrement = 1
 
         //set the progress of the seekbar based on the system's brightness
         seekbar.setOnSeekBarChangeListener(object :
             SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                //Display the current progress
+                textPerc.text = "${progress}%"
                 Log.d("SeekBar","onProgressChanged 값 변경 중 : progress [${progress}], fromUser [${fromUser}]")
+
+                //Change the screen brightness
+                if(canWrite) {
+                    setBrightness(progress)
+                }
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -48,4 +68,45 @@ class MainActivity : AppCompatActivity() {
         )
 
     }
+
+}
+
+// Extension property to get write system settings permission status
+val Context.canWrite:Boolean
+get() {
+    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+        return Settings.System.canWrite(this)
+    }else{
+        return true
+    }
+}
+
+// Extension function to allow write system settings
+fun Context.allowWritePermission() {
+    if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+        val intent = Intent(
+            Settings.ACTION_MANAGE_WRITE_SETTINGS,
+            Uri.parse("package:$packageName")
+        )
+        startActivity(intent)
+    }
+}
+
+// Extension property to get screen brightness programmatically
+val Context.brightness:Int
+    get() {
+        return Settings.System.getInt(
+            this.contentResolver,
+            Settings.System.SCREEN_BRIGHTNESS,
+            0
+        )
+    }
+
+// Extension method to set screen brightness programmatically
+fun Context.setBrightness(value:Int):Unit{
+    Settings.System.putInt(
+        this.contentResolver,
+        Settings.System.SCREEN_BRIGHTNESS,
+        value
+    )
 }
