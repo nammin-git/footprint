56dacff076f13884964da0398a13fec4c9864379
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-06-30 23:57:43 +0900

    Add files via upload

---
+++ mango/src/java/main/RVAdapter.kt
@@ -0,0 +1,20 @@
+package com.mgprogect.mango
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RVAdapter() : RecyclerView.Adapter<RVAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RVAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
