8153410a1bd7ed67aa07881d63bed5d14d83b98a
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-07-05 23:21:50 +0900

    Add files via upload

--- mango/src/java/main/MainActivity.kt
+++ mango/src/java/main/MainActivity.kt
@@ -2,6 +2,9 @@ package com.mgprogect.mango
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +24,29 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-        //리사이클러뷰 연결
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/LJYrI90QBA",
+                "https://mp-seoul-image-production-s3.mangoplate.com/37208_1620286045944187.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "그랜드애플"
+            )
+        )
 
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/LJYrI90QBA",
+                "https://mp-seoul-image-production-s3.mangoplate.com/37208_1620286045944187.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "그랜드애플"
+            )
+        )
+
+
+        //리사이클러뷰 연결
+        val recyclerview = findViewById<RecyclerView>(R.id.rv)
+        val rvAdapter = RVAdapter(items)
+        recyclerview.adapter = rvAdapter
+
+        //그리드레이아웃 적용
+        recyclerview.layoutManager = GridLayoutManager(this, 2)
     }
 }
