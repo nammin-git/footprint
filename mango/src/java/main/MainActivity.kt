03e0a113068b667402f6c40139894fd36db178b0
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-07-11 00:54:42 +0900

    Add files via upload

--- mango/src/java/main/MainActivity.kt
+++ mango/src/java/main/MainActivity.kt
@@ -26,17 +26,49 @@ class MainActivity : AppCompatActivity() {
 
         items.add(
             ContentModel(
-                "https://www.mangoplate.com/restaurants/LJYrI90QBA",
-                "https://mp-seoul-image-production-s3.mangoplate.com/37208_1620286045944187.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-                "그랜드애플"
+                "https://www.mangoplate.com/restaurants/TXkjVTA_MXiL",
+                "https://mp-seoul-image-production-s3.mangoplate.com/978259_1609066259920123.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "톤쇼우"
             )
         )
 
         items.add(
             ContentModel(
-                "https://www.mangoplate.com/restaurants/LJYrI90QBA",
-                "https://mp-seoul-image-production-s3.mangoplate.com/37208_1620286045944187.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-                "그랜드애플"
+                "https://www.mangoplate.com/restaurants/yNGGQSGw7x",
+                "https://mp-seoul-image-production-s3.mangoplate.com/added_restaurants/394407_1453616947181.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "할매국밥"
+            )
+        )
+
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/yNGGQSGw7x",
+                "https://mp-seoul-image-production-s3.mangoplate.com/added_restaurants/394407_1453616947181.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "할매국밥"
+            )
+        )
+
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/eLq_Q72bscee",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1129699_1620482534399327.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "해목"
+            )
+        )
+
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/s3HTCj99t-",
+                "https://mp-seoul-image-production-s3.mangoplate.com/577068_1506913302368580.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "신발원"
+            )
+        )
+
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/2eD8cCEFZL5n",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1037067_1614225069122048.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "포르타나"
             )
         )
 
