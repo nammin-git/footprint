518c9b09387e41d7a514f550550fe55db15a0de0
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-07-31 20:10:27 +0900

    Add files via upload

--- mango/src/java/main/SignupActivity.kt
+++ mango/src/java/main/SignupActivity.kt
@@ -1,11 +1,44 @@
 package com.mgprogect.mango
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        private lateinit var auth: FirebaseAuth
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        auth = Firebase.auth
+
+        val signupBtn = findViewById<Button>(R.id.signupBtn)
+        signupBtn.setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.emailArea)
+            val password = findViewById<EditText>(R.id.passwordArea)
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+
+                    }
+                }
+
+        }
+
     }
 }
