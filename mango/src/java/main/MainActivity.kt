0016a6907bc4807a5f17459a2ab49481213f629f
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-08-10 00:00:56 +0900

    Add files via upload

--- mango/src/java/main/MainActivity.kt
+++ mango/src/java/main/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +18,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bookmarkButton = findViewById<TextView>(R.id.bookmarkBtn)
+        bookmarkButton.setOnClickListener {
+
+            val intent = Intent(this, BookmarkActivity::class.java)
+            startActivity(intent)
+
+        }
+
 
         //리스트에 데이터 추가
         items.add(
