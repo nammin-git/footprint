6d952b19bec6c1e159a6ebe4967fac0744682ea7
nammin-git <80144205+nammin-git@users.noreply.github.com>
2022-08-14 22:41:58 +0900

    Add files via upload

--- mango/src/java/main/BookmarkActivity.kt
+++ mango/src/java/main/BookmarkActivity.kt
@@ -3,6 +3,8 @@ package com.mgprogect.mango
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
@@ -15,6 +17,8 @@ class BookmarkActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    private val contentModel = mutableListOf<ContentModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         auth = Firebase.auth
@@ -22,9 +26,18 @@ class BookmarkActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bookmark)
 
+
         // Write a message to the database
         val database = Firebase.database
         val myBookmarkRef = database.getReference("bookmark_ref")
+
+
+        //recyclerview
+        val recyclerview = findViewById<RecyclerView>(R.id.rv)
+        val rvAdapter = RVAdapter(baseContext, contentModel)
+        recyclerview.adapter = rvAdapter
+        recyclerview.layoutManager = GridLayoutManager(this, 2)
+
 
         myBookmarkRef
             .child(auth.currentUser!!.uid)
@@ -32,7 +45,10 @@ class BookmarkActivity : AppCompatActivity() {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     for (dataModel in snapshot.children) {
                         Log.d("Datamodel", dataModel.toString())
+
+                        contentModel.add(dataModel.getValue(ContentModel::class.java)!!)
                     }
+                    rvAdapter.notifyDataSetChanged()
                 }
 
                 override fun onCancelled(error: DatabaseError) {
