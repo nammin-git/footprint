2f3208df7b685f76dff6df877b4b803df7a1f7d0
Meggy <nammk0121@gmail.com>
2022-08-21 23:57:08 +0900

    second commit

--- app/src/main/java/com/mgprogect/d_dayapp/MainActivity.kt
+++ app/src/main/java/com/mgprogect/d_dayapp/MainActivity.kt
@@ -2,13 +2,28 @@ package com.mgprogect.d_dayapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import com.mgprogect.d_dayapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //header의 버튼 클릭 시 동작
+        val plusListBtn = findViewById<ImageButton>(R.id.plus_list_btn)
+        val myAccountBtn = findViewById<ImageButton>(R.id.my_account_btn)
+
+        plusListBtn.setOnClickListener{
+            //리스트를 만듦
+        }
+
+        myAccountBtn.setOnClickListener {
+            //개인 정보 화면으로 이동
+
+        }
 
     }
 }
